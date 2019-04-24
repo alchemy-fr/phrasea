c794b2a0c667cce5eacad91a5de004f5e033e4b1
Arthur de Moulins <arthurdemoulins@gmail.com>
2019-04-24 15:12:04 +0200

    circleci: install dev dependencies

--- bin/test.sh
+++ bin/test.sh
@@ -4,5 +4,7 @@ set -e
 
 export APP_ENV=test
 
+docker-compose -f docker-compose.yml run --rm upload_php composer install
 docker-compose -f docker-compose.yml run --rm upload_php bin/phpunit
+docker-compose -f docker-compose.yml run --rm auth_php composer install
 docker-compose -f docker-compose.yml run --rm auth_php bin/phpunit
