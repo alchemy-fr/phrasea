2687cd9f20bd52ba34d8be7b5070e84dd945fd73
Arthur de Moulins <arthurdemoulins@gmail.com>
2019-05-29 14:38:59 +0200

    setup different rabbitmq vhost for upload and auth services

--- bin/install.sh
+++ bin/install.sh
@@ -4,17 +4,24 @@ BASEDIR=$(dirname $0)
 
 . "$BASEDIR/load.env.sh"
 
-export APP_ENV=prod
-
 set -ex
 
-docker-compose -f docker-compose.yml up -d \
-    && sleep 10 \
-    && docker-compose -f docker-compose.yml run --rm upload_php /bin/sh -c \
-        "bin/console rabbitmq:setup-fabric" \
-    && docker-compose -f docker-compose.yml run --rm upload_php /bin/sh -c \
-        "bin/console doctrine:database:create --if-not-exists; bin/console doctrine:schema:update -f" \
-    && docker-compose -f docker-compose.yml run --rm auth_php /bin/sh -c \
-        "bin/console doctrine:database:create --if-not-exists; bin/console doctrine:schema:update -f" \
-    && docker-compose -f docker-compose.yml run --rm auth_php /bin/sh -c \
-        "bin/console app:create-client ${CLIENT_ID} --random-id=${CLIENT_RANDOM_ID} --secret=${CLIENT_SECRET} --grant-type password; bin/console app:create-user --update-if-exist ${DEFAULT_USER_EMAIL} -p ${DEFAULT_USER_PASSWORD}"
+export APP_ENV=prod
+CONF="-f docker-compose.yml"
+
+docker-compose ${CONF} up -d
+
+sleep 10
+
+docker-compose ${CONF} exec rabbitmq /bin/sh -c \
+    "rabbitmqctl add_vhost auth; rabbitmqctl add_vhost upload; rabbitmqctl set_permissions -p auth ${RABBITMQ_DEFAULT_USER} '.*' '.*' '.*'; rabbitmqctl set_permissions -p upload ${RABBITMQ_DEFAULT_USER} '.*' '.*' '.*'"
+docker-compose ${CONF} exec upload_php /bin/sh -c \
+    "bin/console rabbitmq:setup-fabric"
+docker-compose ${CONF} exec auth_php /bin/sh -c \
+    "bin/console rabbitmq:setup-fabric"
+docker-compose ${CONF} exec upload_php /bin/sh -c \
+    "bin/console doctrine:database:create --if-not-exists; bin/console doctrine:schema:update -f"
+docker-compose ${CONF} exec auth_php /bin/sh -c \
+    "bin/console doctrine:database:create --if-not-exists; bin/console doctrine:schema:update -f"
+docker-compose ${CONF} exec auth_php /bin/sh -c \
+    "bin/console app:create-client ${CLIENT_ID} --random-id=${CLIENT_RANDOM_ID} --secret=${CLIENT_SECRET} --grant-type password; bin/console app:create-user --update-if-exist ${DEFAULT_USER_EMAIL} -p ${DEFAULT_USER_PASSWORD}"
