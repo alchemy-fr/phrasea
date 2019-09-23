65213c420d367c9e00bd82b133f6aa8401c7e65f
Arthur de Moulins <4rthem@users.noreply.github.com>
2019-09-23 13:30:12 +0200

    add weblate container, setup i18n for uploader client (#5)
    * add weblate container, setup i18n for uploader client
    
    * Translated using Weblate (French)
    
    Currently translated at 100.0% (7 of 7 strings)
    
    Translation: phraseanet/uploader_client
    Translate-URL: http://weblate.alchemy.local/projects/phraseanet/uploader_client/fr/
    
    * Translated using Weblate (Spanish)
    
    Currently translated at 85.7% (6 of 7 strings)
    
    Translation: phraseanet/uploader_client
    Translate-URL: http://weblate.alchemy.local/projects/phraseanet/uploader_client/es/

---
+++ uploader/client/src/locales/i18n.js
@@ -0,0 +1,39 @@
+import i18next from 'i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import { initReactI18next } from "react-i18next";
+import * as languages from './';
+const i18n = i18next.createInstance();
+
+const ns = 'translation';
+
+const resources = {};
+Object.keys(languages).forEach(l => {
+    resources[l] = {
+        [ns]: languages[l],
+    };
+});
+
+i18n
+    .use(LanguageDetector)
+    .use(initReactI18next)
+    .init({
+        debug: false,
+        resources,
+        fallbackLng: 'en',
+        ns: [ns],
+        defaultNs: ns,
+        detection: {
+            order: ['querystring', 'navigator'],
+            lookupQuerystring: 'lng'
+        },
+        react: {
+            bindI18n: 'languageChanged',
+            bindI18nStore: '',
+            transEmptyNodeValue: '',
+            transSupportBasicHtmlNodes: true,
+            transKeepBasicHtmlNodesFor: ['br', 'strong', 'i'],
+            useSuspense: false,
+        }
+    });
+
+export default i18n;
