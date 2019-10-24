270d7de17ce555dd8c9cde92c3a3e436048913a8
Arthur de Moulins <arthurdemoulins@gmail.com>
2019-10-24 18:21:49 +0200

    remove temporarily circle ci react test

--- bin/test.sh
+++ bin/test.sh
@@ -25,10 +25,11 @@ for s in ${SF_SERVICES}; do
     docker-compose$FILE run -T --user app --rm ${s} /bin/sh -c "composer install --no-interaction && bin/console doctrine:schema:update -f && bin/phpunit"
 done
 
-REACT_SERVICES="
-expose_front_dev
-"
-for s in ${REACT_SERVICES}; do
-    # No use of $FILE because we need to load _dev containers (defined in docker-compose.override.yml)
-    docker-compose run -T --rm ${s} /bin/sh -c "CI=1 yarn test"
-done
+# TODO make this work in CircleCI (which has no mounted volumes)
+#REACT_SERVICES="
+#expose_front_dev
+#"
+#for s in ${REACT_SERVICES}; do
+#    # No use of $FILE because we need to load _dev containers (defined in docker-compose.override.yml)
+#    docker-compose run -T --rm ${s} /bin/sh -c "CI=1 yarn test"
+#done
