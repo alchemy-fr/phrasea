ad0083f0a2c7d27c636c0406938c20f27b4947f6
Arthur de Moulins <arthurdemoulins@gmail.com>
2019-11-13 18:38:04 +0100

    fix tests, security upgrade symfony in expose/api

--- expose/api/tests/AssetUploadTest.php
+++ expose/api/tests/AssetUploadTest.php
@@ -29,7 +29,6 @@ class AssetUploadTest extends AbstractTestCase
         $this->assertRegExp('#^[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}$#', $json['id']);
         $this->assertArrayHasKey('size', $json);
         $this->assertSame(846, $json['size']);
-        $this->assertRegExp('#^http://localhost#', $json['url']);
 
         /** @var EntityManagerInterface $em */
         $em = self::$container->get(EntityManagerInterface::class);
