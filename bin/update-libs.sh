5c7d45aed4e13f3ad05f00bdf4c3d7578505b32c
Arthur de Moulins <arthurdemoulins@gmail.com>
2020-04-02 17:35:56 +0200

    fallback to cp when rsync is not installed

--- bin/update-libs.sh
+++ bin/update-libs.sh
@@ -10,6 +10,11 @@ for f in ${SYMFONY_PROJECTS}; do
     rm -rf "${BASEDIR}/${f}/__lib"
     mkdir -p "${BASEDIR}/${f}/__lib"
 
-    rsync -a "${BASEDIR}/lib/" "${BASEDIR}/${f}/__lib/" --exclude=vendor
+    if hash rsync 2>/dev/null; then
+        rsync -a "${BASEDIR}/lib/" "${BASEDIR}/${f}/__lib/" --exclude=vendor
+    else
+        cp -r "${BASEDIR}/lib/"* "${BASEDIR}/${f}/__lib/"
+    fi
+
     echo "$f Synced."
 done
