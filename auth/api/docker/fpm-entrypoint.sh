50016ee486b03db03e93fd8f089f9d45f99cf4da
Arthur de Moulins <arthurdemoulins@gmail.com>
2020-04-07 18:28:42 +0200

    warmup prod cache to use /configs/config.json

--- auth/api/docker/fpm-entrypoint.sh
+++ auth/api/docker/fpm-entrypoint.sh
@@ -4,4 +4,9 @@ if [ ${XDEBUG_ENABLED} == "1" ]; then
     docker-php-ext-enable xdebug
 fi
 
+# Must refresh cache to interpret /configs/config.json
+if [ "${APP_ENV}" == "prod" ]; then
+    su app /bin/sh -c "bin/console cache:clear"
+fi
+
 exec "$@"
