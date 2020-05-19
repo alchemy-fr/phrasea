902578bbdc48d35d9991c60dd74c6d99a0fda913
Arthur de Moulins <arthurdemoulins@gmail.com>
2020-05-19 16:55:38 +0200

    add admin scraper test

---
+++ expose/api/tests/AdminTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests;
+
+use Alchemy\AdminBundle\Tests\AbstractAdminTest;
+
+class AdminTest extends AbstractAdminTest
+{
+    public function testAdmin()
+    {
+        $this->testAllPages();
+    }
+}
---
+++ lib/admin-bundle/Tests/AbstractAdminTest.php
@@ -0,0 +1,86 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Alchemy\AdminBundle\Tests;
+
+use Alchemy\RemoteAuthBundle\Model\RemoteUser;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\BrowserKit\Cookie;
+use Symfony\Component\DomCrawler\Crawler;
+use Symfony\Component\Security\Guard\Token\PostAuthenticationGuardToken;
+
+abstract class AbstractAdminTest extends WebTestCase
+{
+    protected KernelBrowser $client;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->client = static::createClient();
+    }
+
+    protected function testAllPages(): void
+    {
+        $this->client->request('GET', '/admin');
+        $this->client->followRedirects();
+        $response = $this->client->getResponse();
+        $this->assertEquals(302, $response->getStatusCode());
+        $this->assertEquals('/admin/login', $response->getTargetUrl());
+
+        $this->client->followRedirects();
+        $this->logIn();
+        $crawler = $this->client->request('GET', '/admin');
+        $response = $this->client->getResponse();
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertRegexp('#^http\://localhost/admin/\?action=list\&entity=.+$#', $this->client->getHistory()->current()->getUri());
+
+        $crawler
+            ->filter('ul.treeview-menu a')
+            ->each(function ($node, $i) {
+                if ('#' !== $href = $node->attr('href')) {
+                    $this->explore($href);
+                }
+            });
+    }
+
+    private function explore(string $path)
+    {
+        $crawler = $this->loadPage($path);
+
+        $crawler
+            ->filter('a.action-new')
+            ->each(function ($node, $i) {
+                if ('#' !== $href = $node->attr('href')) {
+                    $this->loadPage($href);
+                }
+            });
+    }
+
+    private function loadPage(string $path): Crawler
+    {
+        $crawler = $this->client->request('GET', $path);
+        $response = $this->client->getResponse();
+        $this->assertEquals(200, $response->getStatusCode(), 'On page: '.$path);
+
+        return $crawler;
+    }
+
+    private function logIn()
+    {
+        $session = self::$container->get('session');
+
+        $user = new RemoteUser('123', 'admin', [
+            'ROLE_SUPER_ADMIN',
+        ]);
+
+        $firewallName = 'admin';
+        $token = new PostAuthenticationGuardToken($user, $firewallName, $user->getRoles());
+        $session->set('_security_'.$firewallName, serialize($token));
+        $session->save();
+
+        $cookie = new Cookie($session->getName(), $session->getId());
+        $this->client->getCookieJar()->set($cookie);
+    }
+}
