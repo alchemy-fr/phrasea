a1484379e992f87e4efeefd02c3915ae19a581f0
Arthur de Moulins <arthurdemoulins@gmail.com>
2020-07-30 16:26:33 +0200

    remove all env var from dashboard

--- dashboard/docker/root/entrypoint.sh
+++ dashboard/docker/root/entrypoint.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-export ALL_ENV=$(env | sort)
+#export ALL_ENV=$(env | sort)
 
 envsubst < /var/app/index.tpl.html > /usr/share/nginx/html/index.html
 envsubst < /var/app/menu.tpl.html > /usr/share/nginx/html/menu.html
