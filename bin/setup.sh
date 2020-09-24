70194e5a4b892f6be5f32009bce88ff62bd10169
Arthur de Moulins <arthurdemoulins@gmail.com>
2020-09-24 15:14:24 +0200

    change grant-type from password to authorization_code in clients

--- bin/setup.sh
+++ bin/setup.sh
@@ -26,7 +26,7 @@ exec_container auth-api-php "bin/setup.sh"
 exec_container auth-api-php "bin/console alchemy:oauth:create-client ${AUTH_ADMIN_CLIENT_ID} \
     --random-id=${AUTH_ADMIN_CLIENT_RANDOM_ID} \
     --secret=${AUTH_ADMIN_CLIENT_SECRET} \
-    --grant-type password \
+    --grant-type authorization_code \
     --redirect-uri ${AUTH_BASE_URL}"
 
 
@@ -39,12 +39,12 @@ exec_container uploader-api-php "bin/setup.sh"
 exec_container auth-api-php "bin/console alchemy:oauth:create-client ${UPLOADER_CLIENT_ID} \
     --random-id=${UPLOADER_CLIENT_RANDOM_ID} \
     --secret=${UPLOADER_CLIENT_SECRET} \
-    --grant-type password"
+    --grant-type authorization_code"
 ## Create OAuth client for Admin
 exec_container auth-api-php "bin/console alchemy:oauth:create-client ${UPLOADER_ADMIN_CLIENT_ID} \
     --random-id=${UPLOADER_ADMIN_CLIENT_RANDOM_ID} \
     --secret=${UPLOADER_ADMIN_CLIENT_SECRET} \
-    --grant-type password \
+    --grant-type authorization_code \
     --grant-type client_credentials \
     --scope user:list \
     --scope group:list \
@@ -71,7 +71,7 @@ exec_container auth-api-php "bin/console alchemy:oauth:create-client ${EXPOSE_CL
 exec_container auth-api-php "bin/console alchemy:oauth:create-client ${EXPOSE_ADMIN_CLIENT_ID} \
     --random-id=${EXPOSE_ADMIN_CLIENT_RANDOM_ID} \
     --secret=${EXPOSE_ADMIN_CLIENT_SECRET} \
-    --grant-type password \
+    --grant-type authorization_code \
     --grant-type client_credentials \
     --scope user:list \
     --scope group:list \
@@ -93,7 +93,7 @@ exec_container notify-api-php "bin/setup.sh"
 exec_container auth-api-php "bin/console alchemy:oauth:create-client ${NOTIFY_ADMIN_CLIENT_ID} \
     --random-id=${NOTIFY_ADMIN_CLIENT_RANDOM_ID} \
     --secret=${NOTIFY_ADMIN_CLIENT_SECRET} \
-    --grant-type password \
+    --grant-type authorization_code \
     --redirect-uri ${NOTIFY_BASE_URL}"
 
 
