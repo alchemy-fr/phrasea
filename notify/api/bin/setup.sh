3f58f0decc07657b1d1a7a46a148cd0f0f124c35
Arthur de Moulins <arthurdemoulins@gmail.com>
2020-10-12 12:28:01 +0200

    fix setup for dev (with no vendor installed)

--- notify/api/bin/setup.sh
+++ notify/api/bin/setup.sh
@@ -4,6 +4,10 @@ set -e
 
 BASEDIR=$(dirname $0)
 
-$BASEDIR/console doctrine:database:create --if-not-exists
-$BASEDIR/console doctrine:schema:update -f
-echo y | $BASEDIR/console doctrine:migrations:version --add --all
+if [ ! -d "${BASEDIR}/../vendor" ]; then
+  (cd "${BASEDIR}/.." && composer install)
+fi
+
+"${BASEDIR}/console" doctrine:database:create --if-not-exists
+"${BASEDIR}/console" doctrine:schema:update -f
+echo y | "${BASEDIR}/console" doctrine:migrations:version --add --all
