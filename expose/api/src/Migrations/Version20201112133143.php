b04169fd7aeffb1ce9623cf6a84faeb2b94adbef
Arthur de Moulins <arthurdemoulins@gmail.com>
2020-11-12 14:33:05 +0100

    PS-266 cascade delete download request on publication delete

---
+++ expose/api/src/Migrations/Version20201112133143.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20201112133143 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('ALTER TABLE download_request DROP CONSTRAINT FK_97E98E5A38B217A7');
+        $this->addSql('ALTER TABLE download_request ADD CONSTRAINT FK_97E98E5A38B217A7 FOREIGN KEY (publication_id) REFERENCES publication (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('ALTER TABLE download_request DROP CONSTRAINT fk_97e98e5a38b217a7');
+        $this->addSql('ALTER TABLE download_request ADD CONSTRAINT fk_97e98e5a38b217a7 FOREIGN KEY (publication_id) REFERENCES publication (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+    }
+}
