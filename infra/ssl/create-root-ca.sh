25c6e561adf9374b350fa3c920c97589ddc6c270
jygaulier <gaulier@alchemy.fr>
2021-02-23 11:12:07 +0100

    create a p12 certificate for local dev with ssl (#71)

--- infra/ssl/create-root-ca.sh
+++ infra/ssl/create-root-ca.sh
@@ -2,9 +2,13 @@
 set -e
 
 mkdir -p ~/ssl/
-openssl genrsa -des3 -out ~/ssl/AlchemyRootCA.key 2048
+read -s -p "Enter passphrase: " pass
+
+openssl genrsa -des3 -passout pass:${pass} -out ~/ssl/AlchemyRootCA.key 2048
 openssl req -x509 -new -nodes -key ~/ssl/AlchemyRootCA.key -sha256 -days 1825 \
     -subj "/C=FR/ST=France/O=Alchemy, Inc./CN=Alchemy" \
+    -passin pass:${pass} \
     -out ~/ssl/AlchemyRootCA.pem
 
 echo "Done."
+
