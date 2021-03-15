1904f8bd0f2ccd2191b0cc515e37163ef4db39c4
Arthur de Moulins <arthurdemoulins@gmail.com>
2021-03-15 16:41:33 +0100

    WIP [ci skip]

--- databox/api/bin/setup.sh
+++ databox/api/bin/setup.sh
@@ -8,10 +8,10 @@ if [ ! -d "${BASEDIR}/../vendor" ]; then
   (cd "${BASEDIR}/.." && composer install)
 fi
 
-"${BASEDIR}/console" fos:elastica:create
-"${BASEDIR}/console" fos:elastica:populate
 "${BASEDIR}/console" rabbitmq:setup-fabric
 "${BASEDIR}/console" doctrine:database:create --if-not-exists
 "${BASEDIR}/console" doctrine:schema:update -f
+"${BASEDIR}/console" fos:elastica:create
+"${BASEDIR}/console" fos:elastica:populate
 echo y | "${BASEDIR}/console" doctrine:migrations:sync-metadata-storage
 echo y | "${BASEDIR}/console" doctrine:migrations:version --add --all
