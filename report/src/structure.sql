409eb8a20e8370dcc74368aad466aec1aeecc778
Arthur de Moulins <4rthem@users.noreply.github.com>
2021-04-09 17:18:44 +0200

    PS-300 fix ELK config (#82)

--- report/src/structure.sql
+++ report/src/structure.sql
@@ -1,10 +1,10 @@
 CREATE TABLE IF NOT EXISTS logs (
   id serial PRIMARY KEY,
-  app_id character(36) not null, /** unique service ID */
-  app_name character(30) not null, /** micro-service name */
-  action character(50) not null,
-  item character(36),
-  user_id character(36),
+  app_id varchar(36) not null, /** unique service ID */
+  app_name varchar(30) not null, /** micro-service name */
+  action varchar(50) not null,
+  item varchar(36),
+  user_id varchar(36),
   payload json,
   event_date timestamp not null default CURRENT_TIMESTAMP,
   created_at timestamp not null default CURRENT_TIMESTAMP
