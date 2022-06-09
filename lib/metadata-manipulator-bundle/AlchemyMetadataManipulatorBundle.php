295b54a7d79ee3a9b305673a4d3ba8df8b455afb
jygaulier <gaulier@alchemy.fr>
2022-06-09 09:39:58 +0200

    first commit

---
+++ lib/metadata-manipulator-bundle/AlchemyMetadataManipulatorBundle.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Alchemy\MetadataManipulatorBundle;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AlchemyMetadataManipulatorBundle extends Bundle
+{
+    public function build(ContainerBuilder $container)
+    {
+    }
+}
