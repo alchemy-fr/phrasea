601ae6e0162fd64899bc99f5dedf644b9bd768f6
jygaulier <gaulier@alchemy.fr>
2022-07-26 17:12:51 +0200

    wip debug ci/exiftool : restore after debug

--- lib/metadata-manipulator-bundle/src/DependencyInjection/AlchemyMetadataManipulatorExtension.php
+++ lib/metadata-manipulator-bundle/src/DependencyInjection/AlchemyMetadataManipulatorExtension.php
@@ -19,8 +19,8 @@ class AlchemyMetadataManipulatorExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-//        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../../config'));
-//        $loader->load('services.yaml');
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../../config'));
+        $loader->load('services.yaml');
     }
 
 }
