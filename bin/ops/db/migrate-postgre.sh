0444b4aa6b2dd8806411385cd313b19350c257f3
Arthur de Moulins <arthurdemoulins@gmail.com>
2022-10-05 11:45:33 +0200

    add postgre migration guide

---
+++ bin/ops/db/migrate-postgre.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -e
+
+. "bin/functions.sh"
+
+load-env
+
+DATE=$(date +"%Y-%m-%d-%H-%M")
+DIR="./tmp/export/postgre/${DATE}"
+
+mkdir -p "${DIR}"
+
+. "bin/ops/db/db.sh"
+
+for d in ${DATABASES}; do
+  DUMP_FILE="${DIR}/${d}.sql"
+  echo "Export of $DUMP_FILE..."
+  exec_container db "pg_dump -U ${POSTGRES_USER} --create ${d}" > ${DUMP_FILE}
+done
+
+echo "Delete DB volumes"
+docker-compose stop db
+docker-compose rm -f db
+docker volume rm ${COMPOSE_PROJECT_NAME}_db_vol
+docker-compose up -d db
+
+for d in ${DATABASES}; do
+  DUMP_FILE="${DIR}/${d}.sql"
+  echo "Re-importing $DUMP_FILE..."
+  exec_container db "psql -U ${POSTGRES_USER}" < ${DUMP_FILE}
+done
---
+++ doc/postgre-migration.md
@@ -0,0 +1,10 @@
+# Upgrade PostgreSQL server version
+
+When upgrading to a newer version of PostgreSQL, you need to export your databases, then change server version, and import data back:
+You can find helpers in `bin/ops/db`
+
+Run this command before any `dc up -d`:
+
+```bash 
+$ bin/ops/db/migrate-postgre.sh
+```
