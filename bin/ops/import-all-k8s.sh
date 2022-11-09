b0d9bd2ac2e44b1a0f8eaf9a27f6cfbe0b2d2e1a
Arthur de Moulins <arthurdemoulins@gmail.com>
2022-11-09 12:55:11 +0100

    improve import-all-k8s.sh

--- bin/ops/import-all-k8s.sh
+++ bin/ops/import-all-k8s.sh
@@ -4,8 +4,25 @@ set -e
 
 function echo_usage() {
     echo "Usage:"
-    echo "  $0 <file> <namespace> <db-user> <database-name-prefix>"
+    echo "  $0 [-r] <file> <namespace> <database-name-prefix>"
+    echo ""
+    echo "  options:"
+    echo "    -r  recreate database"
 }
+
+while getopts ":hr" option; do
+  shift $(($OPTIND - 1))
+  case $option in
+    h) # display Help
+      echo_usage
+      exit;;
+    r) # Enter a name
+      RECREATE=1;;
+    \?) # Invalid option
+    echo "Error: Invalid option"
+    exit;;
+  esac
+done
 
 if [ -z "$1" ]; then
   echo "Missing file to import."
@@ -17,16 +34,10 @@ if [ -z "$2" ]; then
   echo_usage
   exit 1
 fi
-if [ -z "$3" ]; then
-  echo "Missing database user."
-  echo_usage
-  exit 1
-fi
 
 PACKAGE="${1}"
 NS="${2}"
-DB_USER="${3}"
-DB_PREFIX="${4}"
+DB_PREFIX="${3}"
 
 if [ ! -f "${PACKAGE}" ]; then
   echo "File ${PACKAGE} does not exist."
@@ -52,7 +63,33 @@ fi
 
 . "bin/ops/db/db.sh"
 
-POD=$(kubectl -n $NS get pod -l tier=postgresql -o jsonpath="{.items[0].metadata.name}")
+POD=db-psql-import
+
+DB_HOST="$(kubectl -n $NS get configmap postgresql-php-config -o "jsonpath={.data['POSTGRES_HOST']}")"
+DB_PORT="$(kubectl -n $NS get configmap postgresql-php-config -o "jsonpath={.data['POSTGRES_PORT']}")"
+DB_USER="$(kubectl -n $NS get secret postgresql-secret -o "jsonpath={.data['POSTGRES_USER']}" | base64 -d)"
+DB_PASSWORD="$(kubectl -n $NS get secret postgresql-secret -o "jsonpath={.data['POSTGRES_PASSWORD']}" | base64 -d)"
+
+kubectl -n $NS delete pod ${POD} || true
+
+cat <<EOF | kubectl -n $NS apply -f -
+apiVersion: v1
+kind: Pod
+metadata:
+  name: ${POD}
+spec:
+  containers:
+  - name: postgresql-client
+    image: jbergknoff/postgresql-client
+    command: [ "/bin/sh", "-c", "--" ]
+    args: [ "while true; do sleep 10; done;" ]
+    env:
+      - name: PGPASSWORD
+        value: "${DB_PASSWORD}"
+EOF
+
+kubectl -n $NS wait --for=condition=Ready pod/${POD}
+
 for d in ${DATABASES}; do
   DUMP_FILE="${DIR}/${d}.sql"
 
@@ -61,13 +98,20 @@ for d in ${DATABASES}; do
     exit 2
   fi
 
-  kubectl -n $NS exec ${POD} -- dropdb -U ${DB_USER} ${DB_PREFIX}${d}
-  kubectl -n $NS exec ${POD} -- createdb -U ${DB_USER} ${DB_PREFIX}${d}
-  kubectl -n $NS exec -i ${POD} -- psql -U ${DB_USER} ${DB_PREFIX}${d} < ${DUMP_FILE} 2> /dev/null > /dev/null
+  CONN_ARGS="-U ${DB_USER} --host ${DB_HOST} --port ${DB_PORT} ${DB_PREFIX}${d}"
+
+  if [ "${RECREATE}" = "1" ]; then
+    kubectl -n $NS exec ${POD} -- dropdb ${CONN_ARGS}
+    echo "[✓] ${d} old database dropped"
+    kubectl -n $NS exec ${POD} -- createdb ${CONN_ARGS}
+    echo "[✓] ${d} new database created"
+  fi
+  kubectl -n $NS exec ${POD} -- psql ${CONN_ARGS} < ${DUMP_FILE}
   echo "[✓] ${d} database imported"
 done
+
+kubectl -n $NS delete pod ${POD} --force
 
 echo "[!] config.json cannot be updated automatically, depending on your infra."
 echo "Don't forget to update your ConfigMap with its content:"
 echo "  $ cat $(realpath ${DIR})/config.json"
-
