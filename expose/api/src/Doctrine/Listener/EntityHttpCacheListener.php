e0e0ef76e0dad8f0a1745f7964540158611e1395
Arthur de Moulins <arthurdemoulins@gmail.com>
2022-11-28 16:12:43 +0100

    PS-508 spread publication reverse proxy invalidation on asset and profile change

--- expose/api/src/Doctrine/Listener/EntityHttpCacheListener.php
+++ expose/api/src/Doctrine/Listener/EntityHttpCacheListener.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace App\Doctrine\Listener;
 
+use App\Entity\Asset;
 use App\Entity\EnvVar;
 use App\Entity\Publication;
+use App\Entity\PublicationProfile;
 use App\Http\Cache\ProxyCachePurger;
 use Doctrine\Common\EventSubscriber;
 use Doctrine\ORM\Event\LifecycleEventArgs;
@@ -30,14 +32,27 @@ class EntityHttpCacheListener implements EventSubscriber
         if ($entity instanceof EnvVar) {
             $this->proxyCachePurger->purgeRoute('global_config');
         } elseif ($entity instanceof Publication) {
-            $this->proxyCachePurger->purgeRoute('api_publications_get_item', [
-                'id' => $entity->getId(),
-            ]);
-            if ($entity->getSlug()) {
-                $this->proxyCachePurger->purgeRoute('api_publications_get_item', [
-                    'id' => $entity->getSlug(),
-                ]);
+            $this->invalidatePublicationCache($entity);
+        } elseif ($entity instanceof Asset) {
+            foreach ($entity->getPublications() as $publication) {
+                $this->invalidatePublicationCache($publication->getPublication());
             }
+        } elseif ($entity instanceof PublicationProfile) {
+            foreach ($entity->getPublications() as $publication) {
+                $this->invalidatePublicationCache($publication);
+            }
+        }
+    }
+
+    private function invalidatePublicationCache(Publication $publication): void
+    {
+        $this->proxyCachePurger->purgeRoute('api_publications_get_item', [
+            'id' => $publication->getId(),
+        ]);
+        if ($publication->getSlug()) {
+            $this->proxyCachePurger->purgeRoute('api_publications_get_item', [
+                'id' => $publication->getSlug(),
+            ]);
         }
     }
 
