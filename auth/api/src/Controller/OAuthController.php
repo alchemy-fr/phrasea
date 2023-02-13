b3c141e49fa2f31a1ef1178891bfeff0a9c746ac
Arthur de Moulins <arthurdemoulins@gmail.com>
2023-02-13 18:13:38 +0100

    revert change

--- auth/api/src/Controller/OAuthController.php
+++ auth/api/src/Controller/OAuthController.php
@@ -79,10 +79,10 @@ class OAuthController extends AbstractIdentityProviderController
         ], UrlGeneratorInterface::ABSOLUTE_URL);
 
         return $this->redirect($resourceOwner->getAuthorizationUrl($redirectUri, [
-            'state' => base64_decode(http_build_query([
+            'state' => http_build_query([
                 'c' =>  $clientId,
                 'r' => $lastRedirectUri,
-            ]))
+            ])
         ]));
     }
 
