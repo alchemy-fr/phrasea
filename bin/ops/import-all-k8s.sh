fe55a759df04f51752a86976ff4da0b644734db2
Arthur de Moulins <arthurdemoulins@gmail.com>
2023-03-09 12:37:08 +0100

    fix import/export with schema structure

--- bin/ops/import-all-k8s.sh
+++ bin/ops/import-all-k8s.sh
@@ -95,7 +95,8 @@ BEGIN
     FOR r IN (SELECT tablename FROM pg_tables WHERE schemaname = current_schema()) LOOP
         EXECUTE 'DROP TABLE IF EXISTS ' || quote_ident(r.tablename) || ' CASCADE';
     END LOOP;
-END $$;EOF
+END $$;
+EOF
 
 for d in ${DATABASES}; do
   DUMP_FILE="${DIR}/${d}.sql"
