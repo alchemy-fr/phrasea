492550ec208788837ee52577f049178631ca157b
jygaulier <gaulier@alchemy.fr>
2023-03-28 20:08:20 +0200

    move phpexiftool tag classes to var/cache/phpexiftool/TagGroup/

--- lib/php/metadata-manipulator-bundle/src/BuildPhpExiftoolClassesCompilerPass.php
+++ lib/php/metadata-manipulator-bundle/src/BuildPhpExiftoolClassesCompilerPass.php
@@ -6,13 +6,14 @@ use PHPExiftool\InformationDumper;
 use PHPExiftool\PHPExiftool;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use PHPExiftool\Driver\TagGroup\ExifTool\ExifToolVersion;
 
 class BuildPhpExiftoolClassesCompilerPass implements CompilerPassInterface
 {
     public function process(ContainerBuilder $container)
     {
-        if (!PHPExiftool::isClassesGenerated()) {
-            PHPExiftool::generateClasses([InformationDumper::LISTOPTION_MWG], ['en']);
+        if(!class_exists(ExifToolVersion::class)) {
+            PHPExiftool::generateClasses([InformationDumper::LISTOPTION_MWG], ['en'], __DIR__ . "/../../../var/cache/phpexiftool");
         }
     }
 }
