0bb180a0b71f321a6c167e06d662ba31fcbaed32
Arthur de Moulins <arthurdemoulins@gmail.com>
2023-04-11 10:20:03 +0200

    remove docker-compose.prod.yml

--- bin/publish.sh
+++ bin/publish.sh
@@ -6,6 +6,5 @@ docker login -u "$DOCKERHUB_USERNAME" -p "$DOCKERHUB_TOKEN"
 
 docker-compose \
     -f docker-compose.yml \
-    -f docker-compose.prod.yml \
     -f docker-compose.report-elk.yml \
   push
