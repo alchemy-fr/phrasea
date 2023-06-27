cf4882044f59c6392dd5d161e810ce378d3ff60c
Arthur de Moulins <arthurdemoulins@gmail.com>
2023-06-27 16:19:41 +0200

    WIP

--- databox/api/rector.php
+++ databox/api/rector.php
@@ -8,19 +8,21 @@ use Rector\Set\ValueObject\LevelSetList;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([
-        __DIR__.'/__lib',
-        __DIR__.'/config',
-        __DIR__.'/public',
-        __DIR__.'/src',
-        __DIR__.'/tests',
+        __DIR__ . '/__lib',
+        __DIR__ . '/config',
+        __DIR__ . '/public',
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
     ]);
 
     // register a single rule
     $rectorConfig->rule(InlineConstructorDefaultToPropertyRector::class);
-    $rectorConfig->importNames();
 
     // define sets of rules
     $rectorConfig->sets([
         LevelSetList::UP_TO_PHP_82,
+        \Rector\Doctrine\Set\DoctrineSetList::ANNOTATIONS_TO_ATTRIBUTES,
+        \Rector\Symfony\Set\SymfonySetList::ANNOTATIONS_TO_ATTRIBUTES,
+        \Rector\Symfony\Set\SensiolabsSetList::FRAMEWORK_EXTRA_61,
     ]);
 };
