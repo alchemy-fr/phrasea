0ab2391b006fb52ebf6e39caea7f71c6239c384c
Arthur de Moulins <arthurdemoulins@gmail.com>
2023-11-28 15:12:46 +0100

    WIP

---
+++ uploader/client/src/index.tsx
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom/client'
+import './scss/index.scss';
+import './i18n/i18n';
+import App from "./App";
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+    <App />
+);
