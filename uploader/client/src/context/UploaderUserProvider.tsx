91b57ae9db4cf7eee5673e4d39e075d407e3fe58
Arthur de Moulins <arthurdemoulins@gmail.com>
2023-12-12 11:00:18 +0100

    bugfix

--- uploader/client/src/context/UploaderUserProvider.tsx
+++ uploader/client/src/context/UploaderUserProvider.tsx
@@ -11,9 +11,11 @@ export default function UploaderUserProvider({children}: Props) {
     const [uploaderUser, setUploaderUser] = React.useState<UploaderUser | undefined>();
 
     React.useEffect(() => {
-        apiClient.get('/me').then(r => {
-            setUploaderUser(r.data);
-        })
+        if (tokens) {
+            apiClient.get('/me').then(r => {
+                setUploaderUser(r.data);
+            });
+        }
     }, [tokens]);
 
     return <UploaderUserContext.Provider value={{
