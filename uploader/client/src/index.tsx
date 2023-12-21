57281dc8bd92babe123d24a111b98bdd9bbd3f7a
Arthur de Moulins <arthurdemoulins@gmail.com>
2023-12-21 13:41:45 +0100

    upgrade liform-react

--- uploader/client/src/index.tsx
+++ uploader/client/src/index.tsx
@@ -2,7 +2,6 @@ import ReactDOM from 'react-dom/client';
 import './scss/index.scss';
 import './locales/i18n';
 import Root from './Root.tsx';
-import React from 'react';
 import {DashboardMenu} from '@alchemy/react-ps';
 import config from './config';
 import {initSentry} from '@alchemy/core';
@@ -10,10 +9,10 @@ import {initSentry} from '@alchemy/core';
 initSentry(config);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
+    <>
         {config.displayServicesMenu && (
             <DashboardMenu dashboardBaseUrl={config.dashboardBaseUrl} />
         )}
         <Root />
-    </React.StrictMode>
+    </>
 );
