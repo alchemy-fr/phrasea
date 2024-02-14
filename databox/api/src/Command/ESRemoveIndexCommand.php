4eeb7a11cbc32a7f43a3b10be97a6b9874833700
Arthur de Moulins <arthurdemoulins@gmail.com>
2024-02-14 15:55:02 +0100

    add es remove index command

---
+++ databox/api/src/Command/ESRemoveIndexCommand.php
@@ -0,0 +1,88 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Command;
+
+use Elastica\Exception\ExceptionInterface;
+use Elastica\Request;
+use FOS\ElasticaBundle\Elastica\Client;
+use FOS\ElasticaBundle\Index\IndexManager;
+use RuntimeException;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use function array_keys;
+use function sprintf;
+
+class ESRemoveIndexCommand extends Command
+{
+    public function __construct(
+        private readonly IndexManager $indexManager,
+        private readonly Client $client,
+    ) {
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        parent::configure();
+
+        $this
+            ->setName('app:es:delete-index')
+            ->addOption('index', 'i', InputOption::VALUE_REQUIRED)
+            ->addOption('remove-olds')
+            ->addOption('olds-only')
+            ->setDescription('Remove index are its aliases');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $indexArg = $input->getOption('index');
+        $indices = null === $indexArg ? array_keys($this->indexManager->getAllIndexes()) : [$indexArg];
+
+        foreach ($indices as $i) {
+            $output->writeln(sprintf('Delete index <comment>%s</comment>', $i));
+            $nbDeleted = 0;
+            $index = $this->indexManager->getIndex($i);
+
+            $oldsOnly = true === $input->getOption('olds-only');
+            $removeOlds = $oldsOnly || true === $input->getOption('remove-olds');
+            $indexName = $index->getName();
+
+            $response = $this->client->request('_aliases');
+            foreach ($response->getData() as $indexKey => $c) {
+                if (isset($c['aliases'][$indexName])) {
+                    if (!$oldsOnly) {
+                        $output->writeln(sprintf('Removing aliased index <comment>%s</comment>', $indexKey));
+                        ++$nbDeleted;
+                        $this->deleteIndex($indexKey);
+                    }
+
+                    continue;
+                }
+
+                if ($removeOlds && 1 === preg_match('#^'.preg_quote($indexName).'_\d{4}-\d{2}-\d{2}-\d{6}$#', $indexKey)) {
+                    $output->writeln(sprintf('Removing old index <comment>%s</comment>', $indexKey));
+                    ++$nbDeleted;
+                    $this->deleteIndex($indexKey);
+                }
+            }
+
+            $output->writeln(sprintf('<info>%d</info> indices removed!', $nbDeleted));
+        }
+
+        return Command::SUCCESS;
+    }
+
+    private function deleteIndex(string $indexName): void
+    {
+        try {
+            $path = $indexName;
+            $this->client->request($path, Request::DELETE);
+        } catch (ExceptionInterface $deleteOldIndexException) {
+            throw new RuntimeException(sprintf('Failed to delete index "%s" with message: "%s"', $indexName, $deleteOldIndexException->getMessage()), 0, $deleteOldIndexException);
+        }
+    }
+}
