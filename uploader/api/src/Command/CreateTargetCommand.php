6343a5a56f3cdf7289cb0552437ee9b1a1875fa7
Arthur M <4rthem@users.noreply.github.com>
2024-11-13 13:07:51 +0100

    PS-710 uploader - add hidden option on target (#481)

--- uploader/api/src/Command/CreateTargetCommand.php
+++ uploader/api/src/Command/CreateTargetCommand.php
@@ -9,6 +9,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class CreateTargetCommand extends Command
@@ -28,6 +29,7 @@ class CreateTargetCommand extends Command
             ->addArgument('slug', InputArgument::REQUIRED, 'The target unique slug')
             ->addArgument('name', InputArgument::REQUIRED, 'The target name')
             ->addArgument('target-url', InputArgument::REQUIRED)
+            ->addOption('hidden', null, InputOption::VALUE_NONE, 'Whether the target is hidden')
             ->setHelp(<<<EOT
 The <info>%command.name%</info> command creates a new upload target.
 
@@ -52,6 +54,7 @@ EOT
         }
 
         $target->setTargetUrl($input->getArgument('target-url'));
+        $target->setHidden((bool) $input->getOption('hidden'));
         $target->setName($input->getArgument('name'));
         $this->em->persist($target);
         $this->em->flush();
