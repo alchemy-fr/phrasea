2d108715b1ada6be3b1d548aaacff2dc83d19c25
Arthur de Moulins <arthurdemoulins@gmail.com>
2024-11-18 21:43:25 +0100

    WIP

--- databox/client/src/components/Media/Asset/AssetView.tsx
+++ databox/client/src/components/Media/Asset/AssetView.tsx
@@ -64,6 +64,7 @@ export default function AssetView({modalIndex, open}: Props) {
 
     const {data, isSuccess} = useModalFetch({
         queryKey,
+        staleTime: 2000,
         queryFn: () =>
             Promise.all([
                 getAsset(assetId!),
