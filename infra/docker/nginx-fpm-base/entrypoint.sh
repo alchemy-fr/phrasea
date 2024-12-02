36e3390d5e000fa90891ac395fc1bb015c182860
Arthur M <4rthem@users.noreply.github.com>
2024-12-02 10:28:11 +0100

    PS-743 Move from X-Frame-Options to Content-Security-Policy: frame-ancestors (#498)

--- infra/docker/nginx-fpm-base/entrypoint.sh
+++ infra/docker/nginx-fpm-base/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-envsubst '${FPM_HOSTNAME} ${UPLOAD_MAX_FILE_SIZE}' < /etc/nginx/tpl/default.conf > /etc/nginx/conf.d/default.conf
+envsubst '${FPM_HOSTNAME},${UPLOAD_MAX_FILE_SIZE},${DASHBOARD_CLIENT_URL}' < /etc/nginx/tpl/default.conf > /etc/nginx/conf.d/default.conf
 
 exec "$@"
