57094b97a6a4f0d094013faec24aae1034a613c6
jygaulier <gaulier@alchemy.fr>
2024-12-23 20:10:02 +0100

    WIP DO NOT REVIEW/MERGE fix build error

--- databox/indexer/src/handlers/phraseanet/types.d.ts
+++ databox/indexer/src/handlers/phraseanet/types.d.ts
@@ -38,7 +38,7 @@ export type ConfigRenditionBuilder = {
 };
 
 export type ConfigPhraseanetSubdef = ConfigPhraseanetSubdefBase & {
-    parent: string;
+    parent?: string|null;
     builders: Record<string, ConfigRenditionBuilder>;
 };
 
