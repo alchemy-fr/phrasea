65b9f0d70140a27714750e38e822863ccf1555cd
aynsix <asr@esokia-webagency.com>
2025-01-20 13:12:03 +0300

    some fix

--- infra/docker/keycloak/entrypoint.sh
+++ infra/docker/keycloak/entrypoint.sh
@@ -2,9 +2,8 @@
 
 set -e
 
-sed -i "s;dashboard_url;$DASHBOARD_CLIENT_URL;g" /opt/keycloak/themes/phrasea/email/html/password-reset.ftl
+sed -i "s;%dashboard_url%;$DASHBOARD_CLIENT_URL;g" /opt/keycloak/themes/phrasea/email/html/password-reset.ftl
 
+sed -i "s;%dashboard_url%;$DASHBOARD_CLIENT_URL;g" /opt/keycloak/themes/phrasea/email/text/password-reset.ftl
 
-sed -i "s;dashboard_url;$DASHBOARD_CLIENT_URL;g" /opt/keycloak/themes/phrasea/email/text/password-reset.ftl
-
-/opt/keycloak/bin/kc.sh start --optimized
+/opt/keycloak/bin/kc.sh $@
--- infra/docker/keycloak/themes/phrasea/email/html/password-reset.ftl
+++ infra/docker/keycloak/themes/phrasea/email/html/password-reset.ftl
@@ -1,4 +1,4 @@
 <#import "template.ftl" as layout>
 <@layout.emailLayout>
-${kcSanitize(msg("passwordResetBodyHtml", link, linkExpiration, realmName, linkExpirationFormatter(linkExpiration), 'dashboard_url'))?no_esc}
+${kcSanitize(msg("passwordResetBodyHtml", link, linkExpiration, realmName, linkExpirationFormatter(linkExpiration), '%dashboard_url%'))?no_esc}
 </@layout.emailLayout>
