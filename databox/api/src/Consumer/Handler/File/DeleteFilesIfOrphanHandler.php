f2934871ad0a980c13626055ef056c956ae6b860
Arthur M <4rthem@users.noreply.github.com>
2025-01-29 15:55:46 +0100

    PS-480 PS-733 add OrphanFileRemover (#521)

--- databox/api/src/Consumer/Handler/File/DeleteFilesIfOrphanHandler.php
+++ databox/api/src/Consumer/Handler/File/DeleteFilesIfOrphanHandler.php
@@ -4,65 +4,21 @@ declare(strict_types=1);
 
 namespace App\Consumer\Handler\File;
 
-use App\Entity\Core\AssetFileVersion;
-use App\Entity\Core\AssetRendition;
-use App\Entity\Core\File;
-use Doctrine\ORM\EntityManagerInterface;
+use App\File\OrphanFileRemover;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
-use Symfony\Component\Messenger\MessageBusInterface;
 
 #[AsMessageHandler]
 readonly class DeleteFilesIfOrphanHandler
 {
     public function __construct(
-        private EntityManagerInterface $em,
-        private MessageBusInterface $bus,
+        private OrphanFileRemover $orphanFileRemover,
     ) {
     }
 
     public function __invoke(DeleteFilesIfOrphan $message): void
     {
-        $repo = $this->em->getRepository(File::class);
         foreach ($message->getIds() as $id) {
-            $file = $repo->find($id);
-            if ($file instanceof File) {
-                if ($this->isFileReferenced($file->getId())) {
-                    continue;
-                }
-
-                $path = null;
-                if (File::STORAGE_S3_MAIN === $file->getStorage()) {
-                    $path = $file->getPath();
-                }
-
-                $this->em->remove($file);
-                $this->em->flush();
-
-                if (null !== $path) {
-                    $this->bus->dispatch(new DeleteFileFromStorage([$path]));
-                }
-            }
+            $this->orphanFileRemover->removeIfOrphan($id);
         }
-    }
-
-    private function isFileReferenced(string $fileId): bool
-    {
-        foreach ([
-            AssetRendition::class,
-            AssetFileVersion::class,
-        ] as $entityClass) {
-            $qb = $this->em->createQueryBuilder();
-            $qb->select('1')
-                ->from($entityClass, 't')
-                ->andWhere('t.file = :fileId')
-                ->setParameter('fileId', $fileId)
-                ->setMaxResults(1);
-
-            if ($qb->getQuery()->getSingleScalarResult() > 0) {
-                return true;
-            }
-        }
-
-        return false;
     }
 }
--- databox/api/src/Doctrine/Listener/FileListener.php
+++ databox/api/src/Doctrine/Listener/FileListener.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Doctrine\Listener;
 
 use Alchemy\MessengerBundle\Listener\PostFlushStack;
+use App\Consumer\Handler\File\DeleteFileFromStorage;
 use App\Consumer\Handler\File\DeleteFilesIfOrphan;
 use App\Entity\Core\Asset;
 use App\Entity\Core\AssetFileVersion;
@@ -25,7 +26,6 @@ readonly class FileListener implements EventSubscriber
     public function preRemove(PreRemoveEventArgs $args): void
     {
         $object = $args->getObject();
-        $em = $args->getObjectManager();
 
         if ($object instanceof Asset) {
             $this->addFileToDelete($object->getSource());
@@ -33,6 +33,15 @@ readonly class FileListener implements EventSubscriber
             $this->addFileToDelete($object->getFile());
         } elseif ($object instanceof AssetFileVersion) {
             $this->addFileToDelete($object->getFile());
+        } elseif ($object instanceof File) {
+            $path = null;
+            if (File::STORAGE_S3_MAIN === $object->getStorage()) {
+                $path = $object->getPath();
+            }
+
+            if (null !== $path) {
+                $this->postFlushStack->addBusMessage(new DeleteFileFromStorage([$path]));
+            }
         }
     }
 
