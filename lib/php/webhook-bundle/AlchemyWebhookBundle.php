773dee934beb23cbb3ec51231f57a83f70732c76
aynsix <asr@esokia-webagency.com>
2025-03-17 14:47:00 +0300

    unneed

--- lib/php/webhook-bundle/AlchemyWebhookBundle.php
+++ lib/php/webhook-bundle/AlchemyWebhookBundle.php
@@ -4,19 +4,8 @@ declare(strict_types=1);
 
 namespace Alchemy\WebhookBundle;
 
-use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-class AlchemyWebhookBundle extends AbstractBundle
+class AlchemyWebhookBundle extends Bundle
 {
-    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
-    {       
-        $services = $container->services();
-        $services
-            ->defaults()
-                ->autowire()
-                ->autoconfigure();
-        
-    }
 }
