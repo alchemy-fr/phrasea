c47c81f6c592b1e4b6a8fb671b069c5c4e8c1f44
aynsix <asr@esokia-webagency.com>
2025-03-17 15:49:36 +0300

    add prepend extension

--- lib/php/webhook-bundle/AlchemyWebhookBundle.php
+++ lib/php/webhook-bundle/AlchemyWebhookBundle.php
@@ -4,8 +4,22 @@ declare(strict_types=1);
 
 namespace Alchemy\WebhookBundle;
 
-use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
-class AlchemyWebhookBundle extends Bundle
+class AlchemyWebhookBundle extends AbstractBundle
 {
+    public function prependExtension(ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        $builder->prependExtensionConfig('framework', [
+            'http_client' => [
+                'scoped_clients' => [
+                    'webhook.client' => [
+                        
+                    ],
+                ],
+            ],
+        ]);
+    }
 }
