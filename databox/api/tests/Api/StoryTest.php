5c0ba64e2e92b436c9d9a5078972829179e30a9a
jygaulier <gaulier@alchemy.fr>
2025-03-18 20:57:07 +0100

    outputs storyAsset on collections api add story tests

---
+++ databox/api/tests/Api/StoryTest.php
@@ -0,0 +1,156 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Api;
+
+use Alchemy\AuthBundle\Tests\Client\KeycloakClientTestMock;
+use ApiPlatform\Symfony\Bundle\Test\Client;
+use App\Entity\Core\Workspace;
+use App\Tests\AbstractSearchTestCase;
+
+class StoryTest extends AbstractSearchTestCase
+{
+    public function testCreateStory(): void
+    {
+        self::enableFixtures();
+
+        $adminAuthorization = 'Bearer '.KeycloakClientTestMock::getJwtFor(KeycloakClientTestMock::ADMIN_UID);
+
+        $client = static::createClient();
+        $collectionId = "";
+        $assetId = "";
+        $this->createStory($client,$collectionId, $assetId);
+
+        $client->request('GET', '/collections/' . urlencode($collectionId), [
+            'headers' => [
+                'Authorization' => $adminAuthorization,
+            ],
+        ]);
+        $this->assertResponseStatusCodeSame(200);
+        $this->assertJsonContains([
+            '@type' => 'collection',
+            'title' => 'Dummy story-collection',
+            'storyAsset' => [
+                '@type' => 'asset',
+                'id' => $assetId,
+            ],
+        ]);
+
+        $client->request('GET', '/assets/' . urlencode($assetId), [
+            'headers' => [
+                'Authorization' => $adminAuthorization,
+            ],
+        ]);
+        $this->assertResponseStatusCodeSame(200);
+        $this->assertJsonContains([
+            '@type' => 'asset',
+            'title' => 'Dummy story-asset',
+            'storyCollection' => [
+                '@type' => 'collection',
+                'id' => $collectionId,
+                'title' => 'Dummy story-collection',
+            ],
+        ]);
+    }
+
+    public function testRemoveStoryByCollection(): void
+    {
+        self::enableFixtures();
+
+        $adminAuthorization = 'Bearer '.KeycloakClientTestMock::getJwtFor(KeycloakClientTestMock::ADMIN_UID);
+
+        $client = static::createClient();
+        $collectionId = "";
+        $assetId = "";
+        $this->createStory($client,$collectionId, $assetId);
+
+        $client->request('DELETE', '/collections/' . urlencode($collectionId), [
+            'headers' => [
+                'Authorization' => $adminAuthorization,
+            ],
+        ]);
+        $this->assertResponseStatusCodeSame(204);
+
+        $client->request('GET', '/collections/' . urlencode($collectionId), [
+            'headers' => [
+                'Authorization' => $adminAuthorization,
+            ],
+        ]);
+        $this->assertResponseStatusCodeSame(404);
+
+        $client->request('GET', '/assets/' . urlencode($assetId), [
+            'headers' => [
+                'Authorization' => $adminAuthorization,
+            ],
+        ]);
+        $this->assertResponseStatusCodeSame(404);
+    }
+
+    public function testRemoveStoryByAsset(): void
+    {
+        self::enableFixtures();
+
+        $adminAuthorization = 'Bearer '.KeycloakClientTestMock::getJwtFor(KeycloakClientTestMock::ADMIN_UID);
+
+        $client = static::createClient();
+        $collectionId = "";
+        $assetId = "";
+        $this->createStory($client,$collectionId, $assetId);
+
+        $client->request('DELETE', '/assets/' . urlencode($assetId), [
+            'headers' => [
+                'Authorization' => $adminAuthorization,
+            ],
+        ]);
+        $this->assertResponseStatusCodeSame(204);
+
+        $client->request('GET', '/assets/' . urlencode($assetId), [
+            'headers' => [
+                'Authorization' => $adminAuthorization,
+            ],
+        ]);
+        $this->assertResponseStatusCodeSame(404);
+
+        $client->request('GET', '/collections/' . urlencode($collectionId), [
+            'headers' => [
+                'Authorization' => $adminAuthorization,
+            ],
+        ]);
+        $this->assertResponseStatusCodeSame(404);
+    }
+
+    private function createStory(Client $client, string &$collectionId, string &$assetId): void
+    {
+        $response = $client->request('POST', '/collections', [
+            'headers' => [
+                'Authorization' => 'Bearer '.KeycloakClientTestMock::getJwtFor(KeycloakClientTestMock::ADMIN_UID),
+            ],
+            'json' => [
+                'title' => 'Dummy story-collection',
+                'workspace' => $this->findIriBy(Workspace::class, [
+                    'slug' => 'test-workspace',
+                ])
+            ],
+        ]);
+        $this->assertResponseStatusCodeSame(201);
+        $collectionId = $response->toArray()['id'];
+        $collectionIri = $response->toArray()['@id'];
+
+        $response = $client->request('POST', '/assets', [
+            'headers' => [
+                'Authorization' => 'Bearer '.KeycloakClientTestMock::getJwtFor(KeycloakClientTestMock::ADMIN_UID),
+            ],
+            'json' => [
+                'title' => 'Dummy story-asset',
+                'workspace' => $this->findIriBy(Workspace::class, [
+                    'slug' => 'test-workspace',
+                ]),
+                'storyCollection' => $collectionIri,
+            ],
+        ]);
+        $this->assertResponseStatusCodeSame(201);
+        $this->assertResponseHeaderSame('content-type', 'application/ld+json; charset=utf-8');
+        $assetId = $response->toArray()['id'];
+    }
+}
