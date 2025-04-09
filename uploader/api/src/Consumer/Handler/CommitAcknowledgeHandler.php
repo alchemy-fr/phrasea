8e2e97964f19ae74b27606b5aff2e9d3623ed5b1
aynsix <asr@esokia-webagency.com>
2025-04-09 13:34:28 +0300

    unneeded

--- uploader/api/src/Consumer/Handler/CommitAcknowledgeHandler.php
+++ uploader/api/src/Consumer/Handler/CommitAcknowledgeHandler.php
@@ -6,19 +6,15 @@ namespace App\Consumer\Handler;
 
 use App\Entity\Asset;
 use App\Entity\Commit;
-use Alchemy\AuthBundle\Security\JwtUser;
 use Doctrine\ORM\EntityManagerInterface;
 use Alchemy\CoreBundle\Util\DoctrineUtil;
 use Symfony\Component\Messenger\MessageBusInterface;
 use Alchemy\NotifyBundle\Notification\NotifierInterface;
-use Alchemy\AuthBundle\Security\Traits\SecurityAwareTrait;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 
 #[AsMessageHandler]
-final class CommitAcknowledgeHandler
+final readonly class CommitAcknowledgeHandler
 {
-    use SecurityAwareTrait;
-
     public function __construct(
         private MessageBusInterface $bus,
         private NotifierInterface $notifier,
@@ -65,18 +61,7 @@ final class CommitAcknowledgeHandler
                 [
                     'assetCount' => $commit->getAssets()->count(),
                 ]
-            );
-          
-            $user = $this->getUser();
-            if ($user instanceof JwtUser) {
-                $this->notifier->sendEmail(
-                    $user->getEmail(),
-                    'uploader-commit-acknowledged',
-                    [
-                        'assetCount' => $commit->getAssets()->count(),
-                    ]
-                );
-            } 
+            );           
         }
     }
 }
