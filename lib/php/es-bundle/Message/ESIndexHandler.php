99a1bbfb59e471ef2091b18f6d9357731965defe
Arthur de Moulins <arthurdemoulins@gmail.com>
2025-04-16 17:22:36 +0200

    restore ESIndexHandler

--- lib/php/es-bundle/Message/ESIndexHandler.php
+++ lib/php/es-bundle/Message/ESIndexHandler.php
@@ -9,28 +9,14 @@ use Alchemy\ESBundle\Indexer\SearchIndexer;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 
 #[AsMessageHandler]
-class ESIndexHandler
+readonly class ESIndexHandler
 {
-    private array $done = [];
-
     public function __construct(private SearchIndexer $searchIndexer)
     {
     }
 
     public function __invoke(ESIndex $message): void
     {
-
-        foreach ($message->getObjects() as $class => $entities) {
-            foreach ($entities as $ids) {
-                foreach ($ids as $id) {
-                    if (isset($this->done[$class][(string) $id])) {
-                        dump(sprintf('Duplicate index operation for %s:%s', $class, $id));
-                    }
-                    $this->done[$class][(string) $id] = true;
-                }
-            }
-        }
-
         $this->searchIndexer->index(
             $message->getObjects(),
             $message->getDepth(),
