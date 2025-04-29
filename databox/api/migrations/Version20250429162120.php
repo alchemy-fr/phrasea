0d97752477c83bb934dd097aff248b5dc0d029af
jygaulier <gaulier@alchemy.fr>
2025-04-29 18:26:33 +0200

    fix migration: regenerate from current master

--- databox/api/migrations/Version20250429162120.php
+++ databox/api/migrations/Version20250429162120.php
@@ -7,10 +7,7 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
-final class Version20250423183712 extends AbstractMigration
+final class Version20250429162120 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -19,15 +16,8 @@ final class Version20250423183712 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
         $this->addSql(<<<'SQL'
-            ALTER TABLE asset ADD story_collection_id UUID DEFAULT NULL
-        SQL);
-        $this->addSql(<<<'SQL'
-            COMMENT ON COLUMN asset.story_collection_id IS '(DC2Type:uuid)'
-        SQL);
-        $this->addSql(<<<'SQL'
-            ALTER TABLE asset ADD CONSTRAINT FK_2AF5A5C717DFDC8 FOREIGN KEY (story_collection_id) REFERENCES collection (id) NOT DEFERRABLE INITIALLY IMMEDIATE
+            DROP INDEX idx_2af5a5c717dfdc8
         SQL);
         $this->addSql(<<<'SQL'
             CREATE UNIQUE INDEX UNIQ_2AF5A5C717DFDC8 ON asset (story_collection_id)
@@ -49,15 +39,6 @@ final class Version20250423183712 extends AbstractMigration
     public function down(Schema $schema): void
     {
         $this->addSql(<<<'SQL'
-            ALTER TABLE asset DROP CONSTRAINT FK_2AF5A5C717DFDC8
-        SQL);
-        $this->addSql(<<<'SQL'
-            DROP INDEX UNIQ_2AF5A5C717DFDC8
-        SQL);
-        $this->addSql(<<<'SQL'
-            ALTER TABLE asset DROP story_collection_id
-        SQL);
-        $this->addSql(<<<'SQL'
             ALTER TABLE collection DROP CONSTRAINT FK_FC4D653263F07DD
         SQL);
         $this->addSql(<<<'SQL'
@@ -65,6 +46,12 @@ final class Version20250423183712 extends AbstractMigration
         SQL);
         $this->addSql(<<<'SQL'
             ALTER TABLE collection DROP story_asset_id
+        SQL);
+        $this->addSql(<<<'SQL'
+            DROP INDEX UNIQ_2AF5A5C717DFDC8
+        SQL);
+        $this->addSql(<<<'SQL'
+            CREATE INDEX idx_2af5a5c717dfdc8 ON asset (story_collection_id)
         SQL);
     }
 }
