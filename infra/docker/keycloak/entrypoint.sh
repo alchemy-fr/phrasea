1a046f04187f0315631de2591323d6ea0737b740
aynsix <asr@esokia-webagency.com>
2025-05-26 18:56:17 +0300

    add backgound image

---
+++ infra/docker/keycloak/entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+set -e
+
+sed -i "s;##background-image##;$KC_LOGIN_BACKGROUND_IMAGE_URL;g" /opt/keycloak/themes/phrasea/login/resources/css/phrasea/login.css
+
+exec  /opt/keycloak/bin/kc.sh "$@"
