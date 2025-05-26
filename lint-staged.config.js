37b7cebb34781bbcfc044d06225ba5fc144cb1fc
Arthur de Moulins <arthurdemoulins@gmail.com>
2025-05-26 14:41:34 +0200

    test commit

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,10 +1,14 @@
+/**
+ * @filename: lint-staged.config.js
+ * @type {import('lint-staged').Configuration}
+ */
 export default {
-    'databox/client/**/*.[jt]sx?': ['cd databox/client && pnpm cs'],
-    'dashboard/client/**/*.[jt]sx?': ['cd dashboard/client && pnpm cs'],
-    'expose/client/**/*.[jt]sx?': ['cd expose/client && pnpm cs'],
+    'databox/client/**/*.(t|j)sx?': ['cd databox/client', 'pnpm cs'],
+    'dashboard/client/**/*.(t|j)sx?': ['cd dashboard/client', 'pnpm cs'],
+    'expose/client/**/*.(t|j)sx?': ['cd expose/client', 'pnpm cs'],
 
-    'configurator/**/*.php': ['cd configurator && composer cs'],
-    'databox/api/**/*.php': ['cd databox/api && composer cs'],
-    'expose/api/**/*.php': ['cd databox/api && composer cs'],
-    'uploader/api/**/*.php': ['cd uploader/api && composer cs'],
+    'configurator/**/*.php': ['cd configurator', 'composer cs'],
+    'databox/api/**/*.php': ['cd databox/api', 'composer cs'],
+    'expose/api/**/*.php': ['cd databox/api', 'composer cs'],
+    'uploader/api/**/*.php': ['cd uploader/api', 'composer cs'],
 }
