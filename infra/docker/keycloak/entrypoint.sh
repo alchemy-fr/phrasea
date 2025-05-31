744d5ba92735c1868f1cec6ad4f7228c63cce6de
aynsix <asr@esokia-webagency.com>
2025-05-31 09:32:01 +0300

    test

--- infra/docker/keycloak/entrypoint.sh
+++ infra/docker/keycloak/entrypoint.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-# sed -i "s;##background-image##;$KC_LOGIN_BACKGROUND_IMAGE_URL;g" /opt/keycloak/themes/phrasea/login/resources/css/phrasea/login.css
+sed -i 's;##background-image##;$KC_LOGIN_BACKGROUND_IMAGE_URL;g' /opt/keycloak/themes/phrasea/login/resources/css/phrasea/login.css
 
 exec  /opt/keycloak/bin/kc.sh "$@"
 
