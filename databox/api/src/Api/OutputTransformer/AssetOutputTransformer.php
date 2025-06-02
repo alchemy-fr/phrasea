deb4dc00d5aa8ee895a74e51f1c80e9bfe7a54e2
Arthur de Moulins <arthurdemoulins@gmail.com>
2025-06-02 12:39:14 +0200

    PS-871 fix asset output novu group

--- databox/api/src/Api/OutputTransformer/AssetOutputTransformer.php
+++ databox/api/src/Api/OutputTransformer/AssetOutputTransformer.php
@@ -90,12 +90,15 @@ class AssetOutputTransformer implements OutputTransformerInterface
             Asset::GROUP_LIST,
         ], $context)) {
             $output->owner = $this->transformUser($data->getOwnerId());
-            if ($user instanceof JwtUser) {
-                $output->topicSubscriptions = $this->notifier->getTopicSubscriptions(
-                    $data->getTopicKeys(),
-                    $user->getId(),
-                );
-            }
+        }
+
+        if ($user instanceof JwtUser && $this->hasGroup([
+            Asset::GROUP_READ,
+        ], $context)) {
+            $output->topicSubscriptions = $this->notifier->getTopicSubscriptions(
+                $data->getTopicKeys(),
+                $user->getId(),
+            );
         }
 
         if ($this->hasGroup([
