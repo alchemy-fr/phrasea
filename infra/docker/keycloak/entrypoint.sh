f2def0817b8625ef0ae1489ad38f059d7e241c59
aynsix <asr@esokia-webagency.com>
2025-06-03 13:16:31 +0300

    test envsubst

--- infra/docker/keycloak/entrypoint.sh
+++ infra/docker/keycloak/entrypoint.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-sed -i 's|background-image|image|g' /opt/keycloak/themes/phrasea/login/resources/css/phrasea/login.css
+envsubst '${KC_LOGIN_BACKGROUND_IMAGE_URL}' < /opt/keycloak/themes/phrasea/login/resources/css/phrasea/login-phrasea.css > /opt/keycloak/themes/phrasea/login/resources/css/phrasea/login.css
 
 exec  /opt/keycloak/bin/kc.sh "$@"
