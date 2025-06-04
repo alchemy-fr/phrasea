0f1f2e3587edb324cf738e014dad151a879f677b
aynsix <asr@esokia-webagency.com>
2025-06-04 20:25:02 +0300

    add logo env

--- infra/docker/keycloak/entrypoint.sh
+++ infra/docker/keycloak/entrypoint.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-sed "s|##KC_LOGIN_CSS_BACKGROUND##|$KC_LOGIN_CSS_BACKGROUND|g" /opt/keycloak/themes/phrasea/login/resources/css/phrasea/login-phrasea.css > /opt/keycloak/themes/phrasea/login/resources/css/phrasea/login.css
+sed -e "s|##KC_LOGIN_CSS_BACKGROUND##|$KC_LOGIN_CSS_BACKGROUND|g" -e "s|##KC_LOGIN_CSS_LOGO_BACKGROUND##|$KC_LOGIN_CSS_LOGO_BACKGROUND|g" < /opt/keycloak/themes/phrasea/login/resources/css/phrasea/login-phrasea.css > /opt/keycloak/themes/phrasea/login/resources/css/phrasea/login.css
 
 exec  /opt/keycloak/bin/kc.sh "$@"
