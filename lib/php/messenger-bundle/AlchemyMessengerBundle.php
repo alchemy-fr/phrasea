4cd304d3cf38bd62b9dcf1e6a17aa05d67717f4c
Arthur de Moulins <arthurdemoulins@gmail.com>
2025-07-02 15:38:32 +0200

    hotfix: override AddErrorDetailsStampListener to reduce error details header size

--- lib/php/messenger-bundle/AlchemyMessengerBundle.php
+++ lib/php/messenger-bundle/AlchemyMessengerBundle.php
@@ -4,8 +4,15 @@ declare(strict_types=1);
 
 namespace Alchemy\MessengerBundle;
 
+use Alchemy\MessengerBundle\DependencyInjection\Compiler\ReplaceAddErrorStampListenerPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class AlchemyMessengerBundle extends Bundle
 {
+    public function build(ContainerBuilder $container): void
+    {
+        parent::build($container);
+        $container->addCompilerPass(new ReplaceAddErrorStampListenerPass());
+    }
 }
