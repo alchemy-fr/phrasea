7d668d78568440baeb7e5ecbc40edf31c86998bc
aynsix <asr@esokia-webagency.com>
2025-08-07 16:51:37 +0300

    test

--- databox/api/src/Command/DocumentationDumperCommand.php
+++ databox/api/src/Command/DocumentationDumperCommand.php
@@ -8,6 +8,7 @@ use Alchemy\RenditionFactory\RenditionBuilderConfigurationDocumentation;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 #[AsCommand('app:documentation:dump')]
@@ -19,10 +20,34 @@ class DocumentationDumperCommand extends Command
         parent::__construct();
     }
 
+    protected function configure(): void
+    {
+        parent::configure();
+
+        $this
+            ->addOption('output-filename', 'f', InputOption::VALUE_REQUIRED, 'The output filename where the documentation will be saved. eg: documentation.md')
+        ;
+    }
+
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $output->writeln('# '.$this->renditionBuilderConfigurationDocumentation::getName());
-        $output->writeln($this->renditionBuilderConfigurationDocumentation->generate());
+        if ($input->getOption('output-filename')) {
+
+            $outputFilename = $input->getOption('output-filename');
+
+            $outputDir = __DIR__.'/../../generatedDoc';
+            mkdir($outputDir, 0755, true);
+
+            file_put_contents(
+                $outputDir.'/'.$outputFilename,
+                '# '.$this->renditionBuilderConfigurationDocumentation::getName()."\n\n".
+                $this->renditionBuilderConfigurationDocumentation->generate()
+            );
+
+            $output->writeln('Documentation saved to '.$outputDir.'/'.$outputFilename);
+        } else {
+            $output->writeln($this->renditionBuilderConfigurationDocumentation->generate());
+        }
 
         return Command::SUCCESS;
     }
