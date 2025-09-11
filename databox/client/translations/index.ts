b54bc5dd9569beb91816d64c10c66dd1939b7eee
Nicolas Maillat <maillat@alchemy.fr>
2025-09-11 10:38:38 +0200

    remove zh locale

--- databox/client/translations/index.ts
+++ databox/client/translations/index.ts
@@ -2,37 +2,31 @@ import en from './en.json';
 import fr from './fr.json';
 import es from './es.json';
 import de from './de.json';
-import zh from './zh.json';
 
 import enApi from '@alchemy/api/translations/en.json';
 import frApi from '@alchemy/api/translations/fr.json';
 import esApi from '@alchemy/api/translations/es.json';
 import deApi from '@alchemy/api/translations/de.json';
-import zhApi from '@alchemy/api/translations/zh.json';
 
 import enNav from '@alchemy/navigation/translations/en.json';
 import frNav from '@alchemy/navigation/translations/fr.json';
 import esNav from '@alchemy/navigation/translations/es.json';
 import deNav from '@alchemy/navigation/translations/de.json';
-import zhNav from '@alchemy/navigation/translations/zh.json';
 
 import enUi from '@alchemy/phrasea-ui/translations/en.json';
 import frUi from '@alchemy/phrasea-ui/translations/fr.json';
 import esUi from '@alchemy/phrasea-ui/translations/es.json';
 import deUi from '@alchemy/phrasea-ui/translations/de.json';
-import zhUi from '@alchemy/phrasea-ui/translations/zh.json';
 
 import enAuth from '@alchemy/react-auth/translations/en.json';
 import frAuth from '@alchemy/react-auth/translations/fr.json';
 import esAuth from '@alchemy/react-auth/translations/es.json';
 import deAuth from '@alchemy/react-auth/translations/de.json';
-import zhAuth from '@alchemy/react-auth/translations/zh.json';
 
 import enForm from '@alchemy/react-form/translations/en.json';
 import frForm from '@alchemy/react-form/translations/fr.json';
 import esForm from '@alchemy/react-form/translations/es.json';
 import deForm from '@alchemy/react-form/translations/de.json';
-import zhForm from '@alchemy/react-form/translations/zh.json';
 
 const enMerged = {
     ...en,
@@ -78,21 +72,9 @@ const deMerged = {
     },
 };
 
-const zhMerged = {
-    ...zh,
-    lib: {
-        ...zhApi.lib,
-        ...zhNav.lib,
-        ...zhUi.lib,
-        ...zhAuth.lib,
-        ...zhForm.lib,
-    },
-};
-
 export {
     enMerged as en,
     frMerged as fr,
     esMerged as es,
     deMerged as de,
-    zhMerged as zh,
 };
--- uploader/client/translations/index.ts
+++ uploader/client/translations/index.ts
@@ -2,25 +2,21 @@ import en from './en.json';
 import fr from './fr.json';
 import es from './es.json';
 import de from './de.json';
-import zh from './zh.json';
 
 import enApi from '@alchemy/api/translations/en.json';
 import frApi from '@alchemy/api/translations/fr.json';
 import esApi from '@alchemy/api/translations/es.json';
 import deApi from '@alchemy/api/translations/de.json';
-import zhApi from '@alchemy/api/translations/zh.json';
 
 import enNav from '@alchemy/navigation/translations/en.json';
 import frNav from '@alchemy/navigation/translations/fr.json';
 import esNav from '@alchemy/navigation/translations/es.json';
 import deNav from '@alchemy/navigation/translations/de.json';
-import zhNav from '@alchemy/navigation/translations/zh.json';
 
 import enAuth from '@alchemy/react-auth/translations/en.json';
 import frAuth from '@alchemy/react-auth/translations/fr.json';
 import esAuth from '@alchemy/react-auth/translations/es.json';
 import deAuth from '@alchemy/react-auth/translations/de.json';
-import zhAuth from '@alchemy/react-auth/translations/zh.json';
 
 const enMerged = {
     ...en,
@@ -58,19 +54,10 @@ const deMerged = {
     },
 };
 
-const zhMerged = {
-    ...zh,
-    lib: {
-        ...zhApi.lib,
-        ...zhNav.lib,
-        ...zhAuth.lib,
-    },
-};
 
 export {
     enMerged as en,
     frMerged as fr,
     esMerged as es,
     deMerged as de,
-    zhMerged as zh,
 };
