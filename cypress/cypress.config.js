7aa50b536f3fe402e2d1edc156ed80c3a7b7dd04
Arthur de Moulins <arthurdemoulins@gmail.com>
2025-10-03 11:23:01 +0200

    WIP

--- cypress/cypress.config.js
+++ cypress/cypress.config.js
@@ -1,22 +1,23 @@
-const { defineConfig } = require('cypress')
+const {defineConfig} = require('cypress')
 
 const outputDir = 'output';
 
 module.exports = defineConfig({
-  reporter: 'junit',
-  reporterOptions: {
-    mochaFile: outputDir + '/results/output-[hash].xml',
-    toConsole: true
-  },
-  screenshotsFolder: outputDir + '/screenshots',
-  videosFolder: outputDir + '/videos',
-  downloadsFolder: outputDir + '/downloads',
-  e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
-    setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
+    reporter: 'junit',
+    reporterOptions: {
+        mochaFile: outputDir + '/results/output-[hash].xml',
+        toConsole: true
     },
-  },
-  chromeWebSecurity: false
+    defaultCommandTimeout: process.env.DEFAULT_COMMAND_TIMEOUT ? parseInt(process.env.DEFAULT_COMMAND_TIMEOUT) : 6000,
+    screenshotsFolder: outputDir + '/screenshots',
+    videosFolder: outputDir + '/videos',
+    downloadsFolder: outputDir + '/downloads',
+    e2e: {
+        // We've imported your old cypress plugins here.
+        // You may want to clean this up later by importing these.
+        setupNodeEvents(on, config) {
+            return require('./cypress/plugins/index.js')(on, config)
+        },
+    },
+    chromeWebSecurity: false
 })
