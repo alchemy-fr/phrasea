643b7e64c2306b9cd9098fe689f4d95073a87128
Arthur de Moulins <arthurdemoulins@gmail.com>
2025-11-05 10:49:35 +0100

    WIP [documentation]

--- lib/php/core-bundle/Documentation/ApiDocumentationGenerator.php
+++ lib/php/core-bundle/Documentation/ApiDocumentationGenerator.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Alchemy\CoreBundle\Documentation;
 
 use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Output\BufferedOutput;
 use Symfony\Component\HttpKernel\KernelInterface;
@@ -30,7 +31,9 @@ final class ApiDocumentationGenerator extends DocumentationGenerator
             'command' => 'api:openapi:export',
         ]);
         $output = new BufferedOutput();
-        $this->application->run($input, $output);
+        if (Command::SUCCESS !== $this->application->run($input, $output)) {
+            throw new \RuntimeException(sprintf('Unable to generate OpenAPI schema: %s', $output->fetch()));
+        }
 
         return $output->fetch();
     }
