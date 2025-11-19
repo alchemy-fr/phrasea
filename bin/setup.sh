54032357a0d6fbc45180d3ca3ac6daf04b67052d
Arthur de Moulins <arthurdemoulins@gmail.com>
2025-11-19 20:33:29 +0100

    move setup to configurator

--- bin/setup.sh
+++ bin/setup.sh
@@ -8,104 +8,32 @@ load-env
 
 set -ex
 
+docker compose up -d traefik
+
+cleanup() {
+    echo "Stopping task..."
+    kill $(jobs -p) 2>/dev/null
+    exit 130
+}
+
+trap cleanup SIGINT SIGTERM
+
+run_container_as configurator "bin/setup.sh $@" app &
+last_pid=$!
+wait $last_pid
+
+pids=()
+run_container_as uploader-api-php "bin/setup.sh" app &
+pids+=($!)
+run_container_as expose-api-php "bin/setup.sh" app &
+pids+=($!)
+run_container_as databox-api-php "bin/setup.sh" app &
+pids+=($!)
+
+for pid in "${pids[@]}"; do
+    wait "$pid"
+done
+
 docker compose up -d
-
-# Wait for services to be ready
-docker compose run --rm dockerize
-
-# Setup Report
-## Create DB
-create_db "${REPORT_DB_NAME}"
-create_db "${KEYCLOAK_DB_NAME}"
-create_db "${KEYCLOAK2_DB_NAME}"
-
-## Create minio bucket
-COMPOSE_PROFILES="${COMPOSE_PROFILES},setup" docker compose run --rm -T --entrypoint "sh -c" minio-mc "\
-  while ! nc -z minio 9000; do echo 'Wait minio to startup...' && sleep 0.1; done; \
-  sleep 5 && \
-  mc config host add minio http://minio:9000 \$MINIO_ACCESS_KEY \$MINIO_SECRET_KEY && \
-  mc mb --ignore-existing minio/$CONFIGURATOR_S3_BUCKET_NAME \
-"
-
-# Setup Uploader
-## Create rabbitmq vhost
-exec_container rabbitmq "rabbitmqctl add_vhost ${UPLOADER_RABBITMQ_VHOST} && rabbitmqctl set_permissions -p ${UPLOADER_RABBITMQ_VHOST} ${RABBITMQ_USER} '.*' '.*' '.*'"
-## Setup container
-exec_container_as uploader-api-php "bin/setup.sh" app
-## Create minio bucket
-COMPOSE_PROFILES="${COMPOSE_PROFILES},setup" docker compose run --rm -T --entrypoint "sh -c" minio-mc "\
-  while ! nc -z minio 9000; do echo 'Wait minio to startup...' && sleep 0.1; done; \
-  sleep 5 && \
-  mc config host add minio http://minio:9000 \$MINIO_ACCESS_KEY \$MINIO_SECRET_KEY && \
-  mc mb --ignore-existing minio/$UPLOADER_S3_BUCKET_NAME \
-"
-
-# Setup Expose
-## Create rabbitmq vhost
-exec_container rabbitmq "rabbitmqctl add_vhost ${EXPOSE_RABBITMQ_VHOST} && rabbitmqctl set_permissions -p ${EXPOSE_RABBITMQ_VHOST} ${RABBITMQ_USER} '.*' '.*' '.*'"
-## Setup container
-exec_container_as expose-api-php "bin/setup.sh" app
-## Create minio bucket
-COMPOSE_PROFILES="${COMPOSE_PROFILES},setup" docker compose run --rm -T --entrypoint "sh -c" minio-mc "\
-  i=0
-  while ! nc -z minio 9000; do \
-    echo 'Wait for minio to startup...'; \
-    echo \$i; \
-    if [ \$i -gt 180 ]; then \
-      echo 'Timeout'; \
-      exit 1; \
-    fi; \
-    i=\$((i+1)); \
-    sleep 0.1; \
-  done; \
-  sleep 3 \
-  && mc config host add minio http://minio:9000 \$MINIO_ACCESS_KEY \$MINIO_SECRET_KEY \
-  && mc mb --ignore-existing minio/$EXPOSE_S3_BUCKET_NAME \
-"
-
-# Setup Databox
-## Create rabbitmq vhost
-exec_container rabbitmq "rabbitmqctl add_vhost ${DATABOX_RABBITMQ_VHOST} && rabbitmqctl set_permissions -p ${DATABOX_RABBITMQ_VHOST} ${RABBITMQ_USER} '.*' '.*' '.*'"
-## Setup container
-exec_container_as databox-api-php "bin/setup.sh" app
-## Create minio bucket
-COMPOSE_PROFILES="${COMPOSE_PROFILES},setup" docker compose run --rm -T --entrypoint "sh -c" minio-mc "\
-  while ! nc -z minio 9000; do echo 'Wait minio to startup...' && sleep 0.1; done; \
-  sleep 5 && \
-  mc config host add minio http://minio:9000 \$MINIO_ACCESS_KEY \$MINIO_SECRET_KEY && \
-  mc mb --ignore-existing minio/$DATABOX_S3_BUCKET_NAME \
-"
-
-## Setup indexer
-## Create Databox OAuth client for indexer
-COMPOSE_PROFILES="${COMPOSE_PROFILES},setup" docker compose run --rm -T --entrypoint "sh -c" minio-mc "\
-  while ! nc -z minio 9000; do echo 'Wait minio to startup...' && sleep 0.1; done; \
-  sleep 5 && \
-  mc config host add minio http://minio:9000 \$MINIO_ACCESS_KEY \$MINIO_SECRET_KEY && \
-  mc mb --ignore-existing minio/${INDEXER_BUCKET_NAME} \
-"
-exec_container rabbitmq "rabbitmqctl add_vhost s3events && rabbitmqctl set_permissions -p s3events ${RABBITMQ_USER} '.*' '.*' '.*'"
-exec_container rabbitmq "\
-  rabbitmqadmin declare exchange --vhost=s3events name=s3events type=direct durable='true' -u ${RABBITMQ_USER} -p ${RABBITMQ_PASSWORD} \
-  && rabbitmqadmin declare queue --vhost=s3events name=s3events auto_delete=false durable='true' -u ${RABBITMQ_USER} -p ${RABBITMQ_PASSWORD} \
-  && rabbitmqadmin declare binding --vhost=s3events source=s3events destination=s3events routing_key='' -u ${RABBITMQ_USER} -p ${RABBITMQ_PASSWORD}"
-COMPOSE_PROFILES="${COMPOSE_PROFILES},setup" docker compose run --rm -T --entrypoint "sh -c" minio-mc "\
-  set -x; \
-  while ! nc -z minio 9000; do echo 'Wait minio to startup...' && sleep 0.1; done; \
-    mc config host add minio http://minio:9000 \$MINIO_ACCESS_KEY \$MINIO_SECRET_KEY \
-    && mc admin config set minio notify_amqp:primary \
-      url="amqp://${RABBITMQ_USER}:${RABBITMQ_PASSWORD}@rabbitmq:5672/s3events" \
-      exchange="s3events" \
-      exchange_type="direct" \
-      durable="on" \
-      delivery_mode=2 \
-    && mc admin service restart minio/ \
-    && (mc event add minio/${INDEXER_BUCKET_NAME} arn:minio:sqs::primary:amqp || echo ok)
-"
-
-docker compose restart keycloak
-docker compose run --rm dockerize -wait http://keycloak:9000/health/ready -timeout 200s
-
-run_container_as configurator "bin/setup.sh $@" app
 
 echo "Done."
---
+++ configurator/src/Service/ServiceWaiter.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Service;
+
+use App\Util\HttpClientUtil;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+final readonly class ServiceWaiter
+{
+    public function __construct(
+        private HttpClientInterface $client,
+    ) {
+    }
+
+    public function waitForService(
+        OutputInterface $output,
+        string $url,
+        int $timeout = HttpClientUtil::DEFAULT_TIMEOUT,
+        int $waitMicroseconds = 200_000,
+        array $successCodes = HttpClientUtil::DEFAULT_SUCCESS_CODES,
+        array $unexpectedCodes = HttpClientUtil::DEFAULT_UNEXPECTED_CODES,
+    ): void {
+        HttpClientUtil::waitForHostHttp(
+            $output,
+            $this->client,
+            $url,
+            $timeout,
+            $waitMicroseconds,
+            $successCodes,
+            $unexpectedCodes
+        );
+    }
+}
