ccfad4854856415124da87be94f41126c49b15c3
Arthur de Moulins <arthurdemoulins@gmail.com>
2025-11-25 11:36:42 +0100

    WIP

--- databox/api/src/Api/Model/Input/SavedSearchInput.php
+++ databox/api/src/Api/Model/Input/SavedSearchInput.php
@@ -4,8 +4,14 @@ declare(strict_types=1);
 
 namespace App\Api\Model\Input;
 
+use Symfony\Component\Validator\Constraints as Assert;
+
 class SavedSearchInput extends AbstractOwnerIdInput
 {
+    #[Assert\NotBlank]
     public ?string $title = null;
     public ?bool $public = null;
+
+    #[Assert\NotNull]
+    public ?array $data = null;
 }
