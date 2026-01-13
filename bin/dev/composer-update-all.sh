e03873dc81f76aad17571c19128f90b62d9c6dfd
Arthur de Moulins <arthurdemoulins@gmail.com>
2026-01-13 16:16:01 +0100

    WIP

--- bin/dev/composer-update-all.sh
+++ bin/dev/composer-update-all.sh
@@ -8,3 +8,8 @@ for a in ${SYMFONY_PROJECTS}; do
   echo " $a:$ $@"
   (cd "$a" && ../..//bin/optimize-composer-docker-cache)
 done
+
+for a in ${PHP_LIBS}; do
+  echo " $a:$ $@"
+  (cd "$a" && composer update)
+done
