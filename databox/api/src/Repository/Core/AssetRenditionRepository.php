62ea9654377c919d28a6bdf1eef7505de15ba368
Arthur de Moulins <arthurdemoulins@gmail.com>
2026-03-12 18:04:05 +0100

    PS-1071 remove asset rendition cache that causes doctrine detached entities

--- databox/api/src/Repository/Core/AssetRenditionRepository.php
+++ databox/api/src/Repository/Core/AssetRenditionRepository.php
@@ -4,12 +4,10 @@ declare(strict_types=1);
 
 namespace App\Repository\Core;
 
-use Alchemy\CoreBundle\Cache\TemporaryCacheFactory;
 use App\Entity\Core\AssetRendition;
 use App\Entity\Core\RenditionDefinition;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
-use Symfony\Contracts\Cache\CacheInterface;
 
 class AssetRenditionRepository extends ServiceEntityRepository
 {
@@ -17,14 +15,10 @@ class AssetRenditionRepository extends ServiceEntityRepository
     final public const string OPT_USED_AS = 'usedAs';
     final public const string WITH_FILE = 'withFile';
 
-    private CacheInterface $cache;
-
     public function __construct(
         ManagerRegistry $registry,
-        TemporaryCacheFactory $temporaryCacheFactory,
     ) {
         parent::__construct($registry, AssetRendition::class);
-        $this->cache = $temporaryCacheFactory->createCache();
     }
 
     /**
@@ -32,40 +26,38 @@ class AssetRenditionRepository extends ServiceEntityRepository
      */
     public function findAssetRenditions(string $assetId, array $options = []): array
     {
-        return $this->cache->get('r_ar_'.$assetId.'_'.serialize($options), function () use ($assetId, $options) {
-            $queryBuilder = $this->createQueryBuilder('t')
-                ->select('t, f')
-                ->addSelect('s')
-                ->innerJoin('t.definition', 's')
-                ->leftJoin('t.file', 'f')
-                ->andWhere('t.asset = :asset')
-                ->setParameter('asset', $assetId)
-                ->addOrderBy('s.priority', 'DESC');
+        $queryBuilder = $this->createQueryBuilder('t')
+            ->select('t, f')
+            ->addSelect('s')
+            ->innerJoin('t.definition', 's')
+            ->leftJoin('t.file', 'f')
+            ->andWhere('t.asset = :asset')
+            ->setParameter('asset', $assetId)
+            ->addOrderBy('s.priority', 'DESC');
 
-            if ($options[self::OPT_DEFINITION_IDS] ?? false) {
-                $queryBuilder
-                    ->andWhere('s.id IN (:def_ids)')
-                    ->setParameter('def_ids', $options[self::OPT_DEFINITION_IDS]);
-            }
-            if ($options[self::OPT_USED_AS] ?? false) {
-                $queryBuilder
-                    ->andWhere(implode(' OR ', array_map(
-                        fn (string $usedAs): string => 's.useAs'.ucfirst($usedAs).' = true',
-                        RenditionDefinition::BUILT_IN_RENDITIONS
-                    )));
-            }
+        if ($options[self::OPT_DEFINITION_IDS] ?? false) {
+            $queryBuilder
+                ->andWhere('s.id IN (:def_ids)')
+                ->setParameter('def_ids', $options[self::OPT_DEFINITION_IDS]);
+        }
+        if ($options[self::OPT_USED_AS] ?? false) {
+            $queryBuilder
+                ->andWhere(implode(' OR ', array_map(
+                    fn (string $usedAs): string => 's.useAs'.ucfirst($usedAs).' = true',
+                    RenditionDefinition::BUILT_IN_RENDITIONS
+                )));
+        }
 
-            if (null !== ($options[self::WITH_FILE] ?? null)) {
-                if ($options[self::WITH_FILE]) {
-                    $queryBuilder->andWhere('t.file IS NOT NULL');
-                } else {
-                    $queryBuilder->andWhere('t.file IS NULL');
-                }
+        if (null !== ($options[self::WITH_FILE] ?? null)) {
+            if ($options[self::WITH_FILE]) {
+                $queryBuilder->andWhere('t.file IS NOT NULL');
+            } else {
+                $queryBuilder->andWhere('t.file IS NULL');
             }
+        }
 
-            return $queryBuilder
-                ->getQuery()
-                ->getResult();
-        });
+        return $queryBuilder
+            ->getQuery()
+            ->getResult();
     }
 }
