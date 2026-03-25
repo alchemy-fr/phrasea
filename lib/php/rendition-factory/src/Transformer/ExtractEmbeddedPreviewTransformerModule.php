a7f5381b454fbc4ae6600100bb93a97448244848
aynsix <asr@esokia-webagency.com>
2026-03-25 12:52:00 +0300

    extract embed

---
+++ lib/php/rendition-factory/src/Transformer/ExtractEmbeddedPreviewTransformerModule.php
@@ -0,0 +1,137 @@
+<?php
+
+namespace Alchemy\RenditionFactory\Transformer;
+
+use Alchemy\MetadataManipulatorBundle\MetadataManipulator;
+use Alchemy\RenditionFactory\Context\TransformationContextInterface;
+use Alchemy\RenditionFactory\DTO\FamilyEnum;
+use Alchemy\RenditionFactory\DTO\InputFileInterface;
+use Alchemy\RenditionFactory\DTO\OutputFile;
+use Alchemy\RenditionFactory\DTO\OutputFileInterface;
+use PHPExiftool\Exiftool;
+use Symfony\Component\Config\Definition\Builder\NodeBuilder;
+
+final readonly class ExtractEmbeddedPreviewTransformerModule implements TransformerModuleInterface
+{
+    public function __construct(
+        private readonly MetadataManipulator $metadataManipulator,
+    ) {
+    }
+
+    public static function getName(): string
+    {
+        return 'extract_embedded_preview';
+    }
+
+    public function getDocumentation(): Documentation
+    {
+        $treeBuilder = TransformerConfigHelper::createBaseTree(self::getName());
+        $this->buildConfiguration($treeBuilder->getRootNode()->children());
+
+        return new Documentation(
+            $treeBuilder,
+            <<<HEADER
+            Extract the embedded preview from a document, if it exists. If the document does not contain an embedded preview, the original document will be returned.
+            HEADER
+        );
+    }
+
+    public function buildConfiguration(NodeBuilder $builder): void
+    {
+        // @formatter:off
+        $builder
+            ->arrayNode('options')
+                ->ignoreExtraKeys(false)
+            ->end()
+        ;
+        // @formatter:on
+    }
+
+    public function transform(InputFileInterface $inputFile, array $options, TransformationContextInterface $context): OutputFileInterface
+    {
+        $eligibleTypes = [
+            'application/illustrator',
+        ];
+
+        if (!in_array($inputFile->getType(), $eligibleTypes)) {
+            return $inputFile->createOutputFile();
+        }
+
+        $phpExiftool = $this->metadataManipulator->getPhpExifTool();
+        $exiftool = new Exiftool($phpExiftool->getLogger());
+        $pathfile = $inputFile->getPath();
+        $outputDir = sys_get_temp_dir().'/'.uniqid('extracted-preview');
+        mkdir($outputDir);
+
+        $command = [
+            '-if',
+            '$photoshopthumbnail',
+            '-b',
+            '-PhotoshopThumbnail',
+            '-w',
+            realpath($outputDir).'/PhotoshopThumbnail%c.jpg',
+            '-execute',
+            '-if',
+            '$jpgfromraw',
+            '-b',
+            '-jpgfromraw',
+            '-w',
+            realpath($outputDir).'/JpgFromRaw%c.jpg',
+            '-execute',
+            '-if',
+            '$previewimage',
+            '-b',
+            '-previewimage',
+            '-w',
+            realpath($outputDir).'/PreviewImage%c.jpg',
+            '-execute',
+            '-if',
+            '$xmp:pageimage',
+            '-b',
+            '-xmp:pageimage',
+            '-w',
+            realpath($outputDir).'/XmpPageimage%c.jpg',
+            '-execute',
+            '-if',
+            '$xmp:thumbnailimage',
+            '-b',
+            '-xmp:thumbnailimage',
+            '-w',
+            realpath($outputDir).'/XmpThumbnailImage%c.jpg',
+            '-common_args',
+            '-q',
+            '-m',
+            $pathfile,
+        ];
+
+        $exiftool->executeCommand($command);
+
+        $files = new \DirectoryIterator($outputDir);
+
+        $selected = null;
+        $size = null;
+
+        foreach ($files as $file) {
+            if ($file->isDir() || $file->isDot()) {
+                continue;
+            }
+
+            if (is_null($selected) || $file->getSize() > $size) {
+                $selected = $file->getPathname();
+                $size = $file->getSize();
+            }
+        }
+
+        if ($selected) {
+            return new OutputFile(
+                $selected,
+                'image/jpeg',
+                FamilyEnum::Image,
+                false
+            );
+        }
+
+        return $inputFile->createOutputFile();
+
+    }
+}
