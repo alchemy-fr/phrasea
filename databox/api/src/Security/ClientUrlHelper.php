1cf1a451471eab0e0c939e831a2ba5029610cf57
Arthur de Moulins <arthurdemoulins@gmail.com>
2026-05-18 10:28:40 +0200

    refactor: rename clientBaseUrl to databoxClientBaseUrl in ClientUrlHelper

--- databox/api/src/Security/ClientUrlHelper.php
+++ databox/api/src/Security/ClientUrlHelper.php
@@ -3,15 +3,13 @@
 namespace App\Security;
 
 use App\Entity\Core\Asset;
-use Symfony\Component\DependencyInjection\Attribute\Autowire;
 
 final readonly class ClientUrlHelper
 {
     private const string UNKNOWN_RENDITION = '_';
 
     public function __construct(
-        #[Autowire(env: 'DATABOX_CLIENT_URL')]
-        private string $clientBaseUrl,
+        private string $databoxClientBaseUrl,
     ) {
     }
 
@@ -19,7 +17,7 @@ final readonly class ClientUrlHelper
     {
         return sprintf(
             '%s/assets?_m=%s',
-            $this->clientBaseUrl,
+            $this->databoxClientBaseUrl,
             urlencode(sprintf('/assets/%s/%s', $asset->getId(), self::UNKNOWN_RENDITION))
         );
     }
