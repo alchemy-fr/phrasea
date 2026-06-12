4ed1edf0d3c6c01b7d0cf6318ce00f4192592500
Arthur de Moulins <arthurdemoulins@gmail.com>
2026-06-12 17:15:51 +0200

    remove masonry

--- databox/client/src/components/AssetList/Layouts/index.ts
+++ databox/client/src/components/AssetList/Layouts/index.ts
@@ -2,17 +2,14 @@ import GridLayout from './Grid/GridLayout';
 import React from 'react';
 import {LayoutProps} from '../types';
 import ListLayout from './List/ListLayout';
-import MasonryLayout from './Masonry/MasonryLayout';
 import 'react-virtualized/styles.css';
 
 export enum Layout {
     List = 'l',
     Grid = 'g',
-    Masonry = 'm',
 }
 
 export const layouts: Record<Layout, React.FC<LayoutProps<any>>> = {
     [Layout.Grid]: GridLayout,
     [Layout.List]: ListLayout,
-    [Layout.Masonry]: MasonryLayout,
 };
