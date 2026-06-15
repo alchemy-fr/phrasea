d472615a87c21783b205323f024ffc0ddae1951a
Aina Sitraka <35221835+aynsix@users.noreply.github.com>
2026-06-15 18:56:46 +0300

    fix databox admin Q (#840)

--- lib/php/admin-bundle/Controller/QueuesListController.php
+++ lib/php/admin-bundle/Controller/QueuesListController.php
@@ -6,14 +6,21 @@ namespace Alchemy\AdminBundle\Controller;
 
 use PhpAmqpLib\Connection\AMQPSSLConnection;
 use PhpAmqpLib\Connection\AMQPStreamConnection;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\NullOutput;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\KernelInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class QueuesListController extends AbstractController
 {
-    public function __construct(private array $queues, private array $rabbitmqConfig)
-    {
+    public function __construct(
+        private array $queues,
+        private array $rabbitmqConfig,
+        private KernelInterface $kernel,
+    ) {
     }
 
     #[Route(path: '/admin/queues/list', name: 'queues_list')]
@@ -45,7 +52,25 @@ class QueuesListController extends AbstractController
         $queuesStatus = [];
 
         foreach ($this->queues as $queueName) {
-            [$queueName, $messageCount, $consumerCount] = $channel->queue_declare($queueName, true);
+            try {
+                [$queueName, $messageCount, $consumerCount] = $channel->queue_declare($queueName, true);
+            } catch (\Exception $e) {
+                $application = new Application($this->kernel);
+                $application->setAutoExit(false);
+                $input = new ArrayInput([
+                    'command' => 'messenger:setup-transports',
+                ]);
+                $code = $application->run($input, new NullOutput());
+
+                if (0 !== $code) {
+                    throw new \RuntimeException(sprintf('Cannot setup transports, command exited with code %d', $code));
+                }
+
+                $connection->getConnection()->reconnect();
+                $channel = $connection->channel();
+                [$queueName, $messageCount, $consumerCount] = $channel->queue_declare($queueName, true);
+            }
+
             $queuesStatus[$queueName] = [
                 'queueName' => $queueName,
                 'messageCount' => $messageCount,
