f6c9d3677d2c76c47a11b0f611a8d6d3e675a75a
Arthur de Moulins <arthurdemoulins@gmail.com>
2026-07-07 22:59:26 +0200

    WIP

--- databox/client/src/index.tsx
+++ databox/client/src/index.tsx
@@ -4,7 +4,6 @@ import './scss/index.scss';
 import Root from './components/Root';
 import './i18n';
 import './lib/leaflet';
-import '@alchemy/react-form/src/styles.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
