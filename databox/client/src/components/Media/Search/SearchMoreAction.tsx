ee517ac1aaec36aece29b14d6e25ef8bb72492d9
Arthur de Moulins <arthurdemoulins@gmail.com>
2026-07-27 18:09:24 +0200

    feat: add save as new search

--- databox/client/src/components/Media/Search/SearchMoreAction.tsx
+++ databox/client/src/components/Media/Search/SearchMoreAction.tsx
@@ -7,8 +7,9 @@ import {useTranslation} from 'react-i18next';
 import SaveIcon from '@mui/icons-material/Save';
 import SaveSearchDialog from './SavedSearch/SaveSearchDialog.tsx';
 import {useModals} from '@alchemy/navigation';
-import {ListItemIcon, MenuItem} from '@mui/material';
+import {Divider, ListItemIcon, MenuItem} from '@mui/material';
 import {MoreActionsButton} from '@alchemy/phrasea-ui';
+import SaveAsIcon from '@mui/icons-material/SaveAs';
 import SearchOffIcon from '@mui/icons-material/SearchOff';
 
 type Props = {
@@ -58,50 +59,85 @@ export default function SearchMoreAction({search}: Props) {
                     horizontal: 'left',
                 }}
             >
-                {closeWrapper => [
-                    <MenuItem
-                        disabled={!search.hasSearch}
-                        key={'clear_search'}
-                        onClick={closeWrapper(() => search.reset())}
-                    >
-                        <ListItemIcon>
-                            <SearchOffIcon />
-                        </ListItemIcon>
-                        {t('search.more_options.clear_search', 'Clear search')}
-                    </MenuItem>,
-                    <MenuItem
-                        key={'save_search'}
-                        // loading={updatingSearch}
-                        disabled={
-                            updatingSearch ||
-                            !search.hasSearch ||
-                            (!!lastSavedChecksum &&
-                                lastSavedChecksum === search.searchChecksum)
-                        }
-                        onClick={closeWrapper(() => {
-                            if (search.searchId) {
-                                updateSearch();
-                            } else {
-                                openModal(SaveSearchDialog, {
-                                    search,
-                                    onCreate: savedSearch => {
-                                        search.setSearchId(savedSearch.id);
-                                        setLastSavedChecksum(
-                                            search.searchChecksum
-                                        );
-                                    },
-                                });
+                {closeWrapper => {
+                    const actions = [
+                        <MenuItem
+                            disabled={!search.hasSearch}
+                            key={'clear_search'}
+                            onClick={closeWrapper(() => search.reset())}
+                        >
+                            <ListItemIcon>
+                                <SearchOffIcon />
+                            </ListItemIcon>
+                            {t(
+                                'search.more_options.clear_search',
+                                'Clear search'
+                            )}
+                        </MenuItem>,
+                        <MenuItem
+                            key={'save_search'}
+                            // loading={updatingSearch}
+                            disabled={
+                                updatingSearch ||
+                                !search.hasSearch ||
+                                (!!lastSavedChecksum &&
+                                    lastSavedChecksum === search.searchChecksum)
                             }
-                        })}
-                    >
-                        <ListItemIcon>
-                            <SaveIcon />
-                        </ListItemIcon>
-                        {search.searchId
-                            ? t('search.update_search', 'Update Search')
-                            : t('search.save_search', 'Save Search')}
-                    </MenuItem>,
-                ]}
+                            onClick={closeWrapper(() => {
+                                if (search.searchId) {
+                                    updateSearch();
+                                } else {
+                                    openModal(SaveSearchDialog, {
+                                        search,
+                                        onCreate: savedSearch => {
+                                            search.setSearchId(savedSearch.id);
+                                            setLastSavedChecksum(
+                                                search.searchChecksum
+                                            );
+                                        },
+                                    });
+                                }
+                            })}
+                        >
+                            <ListItemIcon>
+                                <SaveIcon />
+                            </ListItemIcon>
+                            {search.searchId
+                                ? t('search.update_search', 'Update Search')
+                                : t('search.save_search', 'Save Search')}
+                        </MenuItem>,
+                    ];
+
+                    if (search.searchId) {
+                        actions.push(<Divider key={'divsaveas'} />);
+                        actions.push(
+                            <MenuItem
+                                key={'save_as_new'}
+                                onClick={closeWrapper(() => {
+                                    openModal(SaveSearchDialog, {
+                                        search,
+                                        onCreate: savedSearch => {
+                                            search.setSearchId(savedSearch.id);
+                                            setLastSavedChecksum(
+                                                search.searchChecksum
+                                            );
+                                        },
+                                    });
+                                })}
+                            >
+                                <ListItemIcon>
+                                    <SaveAsIcon />
+                                </ListItemIcon>
+                                {t(
+                                    'search.more_options.save_as_new',
+                                    'Save as new search'
+                                )}
+                            </MenuItem>
+                        );
+                    }
+
+                    return actions;
+                }}
             </MoreActionsButton>
         </>
     );
