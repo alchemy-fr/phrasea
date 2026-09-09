fd90481b4b04c3d0a5828f94efad3093c3bc67a8
Arthur de Moulins <arthurdemoulins@gmail.com>
2026-09-09 12:58:04 +0200

    update project configuration and dependencies, improve cache handling

--- bin/dev/composer-update-all.sh
+++ bin/dev/composer-update-all.sh
@@ -4,9 +4,14 @@
 
 set -e
 
+if ! bin/dev/sf-all.sh rm -rf var/cache/test var/cache/dev; then
+  echo "Retrying composer install with sudo..."
+  sudo bin/dev/sf-all.sh rm -rf var/cache/test var/cache/dev
+fi
+
 for a in ${SYMFONY_PROJECTS}; do
   echo " $a:$ $@"
-  (cd "$a" && ../..//bin/optimize-composer-docker-cache)
+  (cd "$a" && ../../bin/optimize-composer-docker-cache)
 done
 
 for a in ${PHP_LIBS}; do
