b956a8c28e8b3f283592d6c19cc59db2abe3a78e
Arthur de Moulins <arthurdemoulins@gmail.com>
2026-09-09 14:22:25 +0200

    update dependencies and improve project scripts for better compatibility

--- bin/dev/composer-update-all.sh
+++ bin/dev/composer-update-all.sh
@@ -4,6 +4,8 @@
 
 set -e
 
+ROOT_DIR="$(cd "$(dirname "$0")/../.." && pwd)"
+
 if ! bin/dev/sf-all.sh rm -rf var/cache/test var/cache/dev; then
   echo "Retrying composer install with sudo..."
   sudo bin/dev/sf-all.sh rm -rf var/cache/test var/cache/dev
@@ -11,7 +13,7 @@ fi
 
 for a in ${SYMFONY_PROJECTS}; do
   echo " $a:$ $@"
-  (cd "$a" && ../../bin/optimize-composer-docker-cache)
+  (cd "$a" && "${ROOT_DIR}/bin/optimize-composer-docker-cache")
 done
 
 for a in ${PHP_LIBS}; do
