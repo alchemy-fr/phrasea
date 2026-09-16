b218eb84b450c3b3c1237753c15da2ebee35a9ac
Arthur de Moulins <arthurdemoulins@gmail.com>
2026-09-16 18:52:14 +0200

    feat(messenger): add MessengerMessage attribute to NotifyAssetTopic and NotifyCollectionTopic

--- databox/api/src/Consumer/Handler/Asset/NotifyAssetTopic.php
+++ databox/api/src/Consumer/Handler/Asset/NotifyAssetTopic.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace App\Consumer\Handler\Asset;
 
+use Alchemy\MessengerBundle\Attribute\MessengerMessage;
+
+#[MessengerMessage('p2')]
 final readonly class NotifyAssetTopic
 {
     public function __construct(
