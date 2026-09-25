b4a7492070ba327956a2c612d53cc2d3797bcfc8
Leonhard Kreissig <github2@blur.leokr.de>
2026-09-25 14:42:58 +0200

    fix(databox-api): switch to inet field type
    - Use inet field type instead of varchar
    - Make down-migration failsafe, so ipv6 values are casted to max 32 chars, which makes them hard to read but retains all information - IPv4 addresses remain untouched
    - Make up-migration re-expand IPv6-Adresses that possibly have been shortened by a down-migration

--- databox/api/migrations/Version20260918190700.php
+++ databox/api/migrations/Version20260918190700.php
@@ -11,18 +11,18 @@ final class Version20260918190700 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return 'Expand IP field in changelog and action log to a length of 39';
+        return 'Use PostgreSQL inet for IP fields in changelog and action log';
     }
 
     public function up(Schema $schema): void
     {
-        $this->addSql('ALTER TABLE action_log ALTER COLUMN ip TYPE VARCHAR(39)');
-        $this->addSql('ALTER TABLE change_log ALTER COLUMN ip TYPE VARCHAR(39)');
+        $this->addSql("ALTER TABLE action_log ALTER COLUMN ip TYPE INET USING regexp_replace(lower(ip), '([0-9a-f]{4})(?=[0-9a-f])', '\\1:', 'g')::inet");
+        $this->addSql("ALTER TABLE change_log ALTER COLUMN ip TYPE INET USING regexp_replace(lower(ip), '([0-9a-f]{4})(?=[0-9a-f])', '\\1:', 'g')::inet");
     }
 
     public function down(Schema $schema): void
     {
-        $this->addSql('ALTER TABLE action_log ALTER COLUMN ip TYPE VARCHAR(32)');
-        $this->addSql('ALTER TABLE change_log ALTER COLUMN ip TYPE VARCHAR(32)');
+        $this->addSql("ALTER TABLE action_log ALTER COLUMN ip TYPE VARCHAR(32) USING regexp_replace(lower(host(ip)), '([0-9a-f]{4}):(?=[0-9a-f])', '\\1', 'g')");
+        $this->addSql("ALTER TABLE change_log ALTER COLUMN ip TYPE VARCHAR(32) USING regexp_replace(lower(host(ip)), '([0-9a-f]{4}):(?=[0-9a-f])', '\\1', 'g')");
     }
 }
